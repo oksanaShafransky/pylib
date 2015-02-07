8a7386dd2f98e31a1b3276733307d4cb80d9b488
Felix Vaisman <felixv@similargroup.com>
2015-02-07 15:41:07 +0200

    tune up for mobile processes

--- jobs/run.py
+++ jobs/run.py
@@ -1,5 +1,17 @@
 
+hadoop_log_dir_template = '/user/%s/tmp/mrjob/%s/output/_logs/history'
+
 def run(job):
-   with job.make_runner() as runner:
-       runner.run()
+    with job.make_runner() as runner:
+        runner.run()
+
+       runner.get_job_name()
+
+
+
        job.post_exec(runner=runner)
+
+
+def get_stats_url(job_name):
+    user_id = job_name.split('.')[1]
+    log_url = 'hdfs://%s' % (hadoop_log_dir_template % (user_id, job_name))
