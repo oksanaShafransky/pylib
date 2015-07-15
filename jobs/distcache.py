cebcef98d50eb690eb1dc6d6113bc1795599768e
Felix Vaisman <felixv@similargroup.com>
2015-07-15 22:16:20 +0300

    p

--- jobs/distcache.py
+++ jobs/distcache.py
@@ -1,6 +1,5 @@
 __author__ = 'Felix'
 
-
 import os
 import pickle
 import tempfile
@@ -66,3 +65,11 @@ def open_file(file_name):
         raise TypeError('snappy files are currently not supported')
     else:
         return open(file_name)
+=======
+from stats import check_output
+
+
+def find_files(path):
+    cmd = ['hadoop', 'fs', '-ls', path]
+    ls_out = check_output()
+>>>>>>> Stashed changes
