8af4dbb8e09b06a9a184c31ebadb7142f64ff4b9
Felix Vaisman <felixv@similargroup.com>
2015-11-15 04:23:14 +0200

    added desktop panel dag

---
+++ sw_airflow/dags/sw/desktop/panel.py
@@ -0,0 +1,54 @@
+__author__ = 'Felix Vaisman'
+
+from datetime import datetime, timedelta
+
+from airflow.models import DAG
+from airflow.operators.sensors import HdfsSensor
+
+from sw.airflow.airflow_etcd import *
+from sw.airflow.operators import DockerBashOperator
+
+DEFAULT_EXECUTION_DIR = '/similargroup/production'
+BASE_DIR = '/similargroup/data'
+DOCKER_MANAGER = 'docker-a02.sg.internal'
+DEFAULT_CLUSTER = 'mrp'
+
+ETCD_ENV_ROOT = {'STAGE': 'v1/dev', 'PRODUCTION': 'v1/production'}
+
+dag_args = {
+    'owner': 'similarweb',
+    'start_date': datetime(15, 11, 13),
+    'depends_on_past': False,
+    'email': ['bigdata@similarweb.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=15)
+}
+
+dag_template_params = {'execution_dir': DEFAULT_EXECUTION_DIR, 'docker_gate': DOCKER_MANAGER,
+                       'base_hdfs_dir': BASE_DIR, 'run_environment': 'PRODUCTION', 'cluster': DEFAULT_CLUSTER}
+
+dag = DAG(dag_id='DesktopDailyPanelReport', default_args=dag_args, params=dag_template_params, schedule_interval=timedelta(days=1))
+
+
+# define stages
+
+group_files_base = '/similargroup/data/stats'
+should_run = HdfsSensor(task_id='GroupFilesReady',
+                        dag=dag,
+                        hdfs_conn_id='hdfs_%s' % DEFAULT_CLUSTER,
+                        filepath='''%s/{{ macros.date_partition(ds) }}/_SUCCESS''' % group_files_base,
+                        execution_timeout=timedelta(minutes=240)
+                        )
+
+
+panel_report = DockerBashOperator(task_id='PanelStats',
+                                  dag=dag,
+                                  docker_name='''{{ params.cluster }}''',
+                                  bash_command='''{{ params.execution_dir }}/analytics/scripts/panel/panelReports.sh -d {{ ds }}'''
+                                  )
+panel_report.set_upstream(should_run)
+
+
+
