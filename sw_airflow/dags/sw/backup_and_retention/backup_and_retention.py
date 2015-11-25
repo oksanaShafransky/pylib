e5692ee3148556433c2d9d62b31a7e56f841da10
kfire <kfire@similarweb.com>
2015-11-25 11:54:55 +0200

    SIM-5305 using new docker image: based on mrp image, with python's modules: snakebite and boto installed

--- sw_airflow/dags/sw/backup_and_retention/backup_and_retention.py
+++ sw_airflow/dags/sw/backup_and_retention/backup_and_retention.py
@@ -10,6 +10,7 @@ DEFAULT_EXECUTION_DIR = '/similargroup/production'
 BASE_DIR = '/similargroup/data'
 DOCKER_MANAGER = 'docker-a02.sg.internal'
 DEFAULT_CLUSTER = 'mrp'
+DOCKER_IMAGE = 'mrp-retention'
 
 dag_args = {
     'owner': 'similarweb',
@@ -23,7 +24,7 @@ dag_args = {
 }
 
 dag_template_params = {'execution_dir': DEFAULT_EXECUTION_DIR, 'docker_gate': DOCKER_MANAGER,
-                       'base_hdfs_dir': BASE_DIR, 'run_environment': 'PRODUCTION', 'cluster': DEFAULT_CLUSTER}
+                       'base_hdfs_dir': BASE_DIR, 'run_environment': 'PRODUCTION', 'cluster': DEFAULT_CLUSTER, 'docker_image': DOCKER_IMAGE}
 
 dag = DAG(dag_id='HdfsBackupAndRetentionDag', default_args=dag_args, params=dag_template_params, schedule_interval=timedelta(days=1))
 
@@ -41,7 +42,7 @@ for i in range(0, len(retention_targets)):
     backup_and_retention_op = \
         DockerBashOperator(task_id='HdfsBackupAndRetentionTask-%s' % (i),
                            dag=dag,
-                           docker_name='''{{ params.cluster }}''',
+                           docker_name='''{{ params.docker_image }}''',
                            bash_command='''python {{ params.execution_dir }}/utils/scripts/backup_and_retention.py --path %s --retention_days %s --log_level %s --dryrun %s''' % (path, retention_period, 'DEBUG', 'True')
                            )
 
