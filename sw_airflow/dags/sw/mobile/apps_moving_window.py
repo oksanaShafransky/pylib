b2d44955df1e468227e60d8515bdc71c8e490717
iddo <iddo.aviram@similarweb.com>
2015-12-02 17:47:55 +0200

    - Bug fix: The end date for the snapshot must not be open

--- sw_airflow/dags/sw/mobile/apps_moving_window.py
+++ sw_airflow/dags/sw/mobile/apps_moving_window.py
@@ -53,7 +53,7 @@ def generate_dags(mode):
         dag_args.update({'start_date': datetime(2015, 11, 29)})
 
     if is_snapshot_dag():
-        dag_args.update({'start_date': datetime(2015, 11, 30)})
+        dag_args.update({'start_date': datetime(2015, 11, 30), 'end_date': datetime(2015, 11, 30)})
 
     dag_template_params_for_mode = dag_template_params.copy()
     mode_dag_template_params = {}
