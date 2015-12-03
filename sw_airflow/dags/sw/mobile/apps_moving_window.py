94144029bf750117f0aa2723d0864bc3bbc10551
iddo <iddo.aviram@similarweb.com>
2015-12-03 18:24:34 +0200

    Bug fix: Fixing the environment type name in the mobile apps' DAG update usage ranks date prod task.

--- sw_airflow/dags/sw/mobile/apps_moving_window.py
+++ sw_airflow/dags/sw/mobile/apps_moving_window.py
@@ -656,7 +656,7 @@ def generate_dags(mode):
             DockerBashOperator(task_id='UpdateUsageRanksDateProd',
                                dag=dag,
                                docker_name='''{{ params.cluster }}''',
-                               bash_command='''{{ params.execution_dir }}/mobile/scripts/dynamic-settings.sh -d {{ ds }} -bd {{ params.base_hdfs_dir }} -m {{ params.mode }} -mt {{ params.mode_type }} -et PRODUCTION_MRP -p usage_ranks -pn UsageRanksProd -um success'''
+                               bash_command='''{{ params.execution_dir }}/mobile/scripts/dynamic-settings.sh -d {{ ds }} -bd {{ params.base_hdfs_dir }} -m {{ params.mode }} -mt {{ params.mode_type }} -et PRODUCTION -p usage_ranks -pn UsageRanksProd -um success'''
                                )
         update_usage_ranks_date_prod.set_upstream(copy_to_prod)
 
