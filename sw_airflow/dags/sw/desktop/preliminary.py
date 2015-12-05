3013741dfdc1d732c2c65c1e33bb2a294e54fb31
Felix Vaisman <felixv@similargroup.com>
2015-12-05 19:20:41 +0200

    moved daily aggregation to preliminary

--- sw_airflow/dags/sw/desktop/preliminary.py
+++ sw_airflow/dags/sw/desktop/preliminary.py
@@ -63,8 +63,9 @@ daily_aggregation = DockerBashOperator(task_id='DailyAggregation',
                                        dag=dag,
                                        docker_name='''{{ params.cluster }}''',
                                        bash_command='''{{ params.execution_dir }}/analytics/scripts/daily/dailyAggregation.sh -d {{ ds }} -p aggregate'''
-                                 )
-daily_aggregation.set_upstream(group_raw, blocked_ips)
+                                       )
+daily_aggregation.set_upstream(group_raw)
+daily_aggregation.set_upstream(blocked_ips)
 
 repair_tables = DockerBashOperator(task_id='RepairDailyTables',
                                    dag=dag,
