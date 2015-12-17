8e7e4c04a7c06533f06cc8dbc082a7f92973d2fb
kfire <kfire@similarweb.com>
2015-12-17 16:31:17 +0200

    retention process:
    setting a pool with 1 slot so instances will not run concurrently
    sorting folders so the ones that only need to be deleted will be handled first, thus clearing storage first (backup to s3 runs much longer)

--- sw_airflow/dags/sw/backup_and_retention/backup_and_retention.py
+++ sw_airflow/dags/sw/backup_and_retention/backup_and_retention.py
@@ -7,24 +7,24 @@ from sw.airflow.airflow_etcd import *
 from sw.airflow.operators import DockerBashOperator
 
 DEFAULT_EXECUTION_DIR = '/similargroup/production'
-BASE_DIR = '/similargroup/data'
 DOCKER_MANAGER = 'docker-a02.sg.internal'
 DEFAULT_CLUSTER = 'mrp'
 DOCKER_IMAGE = 'mrp-retention'
 
 dag_args = {
     'owner': 'similarweb',
-    'start_date': datetime(2015, 12, 7),
-    'depends_on_past': False,
+    'start_date': datetime(2015, 12, 17),
+    'depends_on_past': True,
     'email': ['bigdata@similarweb.com'],
     'email_on_failure': True,
     'email_on_retry': False,
     'retries': 0,
+    'pool': 'Retention',
     'retry_delay': timedelta(minutes=15)
 }
 
 dag_template_params = {'execution_dir': DEFAULT_EXECUTION_DIR, 'docker_gate': DOCKER_MANAGER,
-                       'base_hdfs_dir': BASE_DIR, 'run_environment': 'PRODUCTION', 'cluster': DEFAULT_CLUSTER, 'docker_image': DOCKER_IMAGE}
+                       'run_environment': 'PRODUCTION', 'cluster': DEFAULT_CLUSTER, 'docker_image': DOCKER_IMAGE}
 
 dag = DAG(dag_id='HdfsBackupAndRetentionDag', default_args=dag_args, params=dag_template_params, schedule_interval=timedelta(days=1))
 
