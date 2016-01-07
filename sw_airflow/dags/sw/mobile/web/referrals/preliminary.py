d5d9b4d8fa0b2ea524484b2007a46f81c545ec6c
amitr <amitr@similargroup.com>
2016-01-07 14:59:25 +0200

    SIM-7123: Killed task should kill jobs cleanup docker etc Temp test for airflow deployment

--- sw_airflow/dags/sw/mobile/web/referrals/preliminary.py
+++ sw_airflow/dags/sw/mobile/web/referrals/preliminary.py
@@ -8,6 +8,10 @@ from airflow.operators.dummy_operator import DummyOperator
 from sw.airflow.airflow_etcd import *
 from sw.airflow.operators import DockerBashOperator
 
+from airflow.models import BaseOperator
+from airflow.utils import apply_defaults
+import time
+
 DEFAULT_EXECUTION_DIR = '/similargroup/production'
 BASE_DIR = '/similargroup/data/mobile-analytics'
 DOCKER_MANAGER = 'docker-a02.sg.internal'
@@ -26,6 +30,19 @@ dag_args = {
     'retry_delay': timedelta(minutes=15)
 }
 
+class TestKillOperator(BaseOperator):
+    ui_color = '#00BFFF'
+
+    @apply_defaults
+    def __init__(self, *args, **kwargs):
+        super(TestKillOperator, self).__init__(*args, **kwargs)
+
+    def execute(self, context):
+        time.sleep(60)
+
+    def on_kill(self):
+        print "Help, i'm being killed"
+
 dag_template_params = {'execution_dir': DEFAULT_EXECUTION_DIR, 'docker_gate': DOCKER_MANAGER,
                        'base_hdfs_dir': BASE_DIR, 'run_environment': 'PRODUCTION', 'cluster': DEFAULT_CLUSTER}
 
@@ -39,11 +56,18 @@ opera_raw_data_ready = EtcdSensor(task_id='OperaRawDataReady',
 )
 
 
+test = TestKillOperator(task_id='TestKillOperator',
+                 dag=dag)
+
+
 filter_malformed_events = DockerBashOperator(task_id='FilterMalformedEvents',
                                      dag=dag,
                                      docker_name='''{{ params.cluster }}''',
                                      bash_command='''{{ params.execution_dir }}/mobile/scripts/web/referrals/preliminary.sh -d {{ ds }} -p filter_malformed_events -env main'''
 )
+
+
+filter_malformed_events.set_upstream(test)
 filter_malformed_events.set_upstream(opera_raw_data_ready)
 
 
