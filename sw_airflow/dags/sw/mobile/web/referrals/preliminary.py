84eb09d8c4a8012194ddaf3c06b5bf9346364c90
amitr <amitr@similargroup.com>
2016-01-07 12:56:11 +0200

    SIM-6579: Referrals Airflow Orchestration Added dependency on past task instances

--- sw_airflow/dags/sw/mobile/web/referrals/preliminary.py
+++ sw_airflow/dags/sw/mobile/web/referrals/preliminary.py
@@ -18,7 +18,7 @@ ETCD_ENV_ROOT = {'STAGE': 'v1/dev', 'PRODUCTION': 'v1/production'}
 dag_args = {
     'owner': 'similarweb',
     'start_date': datetime(2015, 12, 1),
-    'depends_on_past': False,
+    'depends_on_past': True,
     'email': ['amitr@similarweb.com'],
     'email_on_failure': True,
     'email_on_retry': False,
