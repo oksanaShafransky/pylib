74cf0414144051b8ef35b35544feb73c33d1efc5
kfire <kfire@similarweb.com>
2016-01-17 20:50:18 +0200

    desktop moving window dag: no need for hbase tables creation to depend on daily estimation

--- sw_airflow/dags/sw/desktop/desktop_moving_window.py
+++ sw_airflow/dags/sw/desktop/desktop_moving_window.py
@@ -88,7 +88,6 @@ def generate_dags(mode):
                            docker_name='''{{ params.cluster }}''',
                            bash_command='''{{ params.execution_dir }}/analytics/scripts/monthly/start-process.sh -d {{ macros.last_interval_day(ds, dag.schedule_interval) }} -bd {{ params.base_hdfs_dir }} -m {{ params.mode }} -mt {{ params.mode_type }} -p tables'''
                            )
-    hbase_tables.set_upstream(daily_aggregation)
 
     daily_incoming = \
         DockerBashOperator(task_id='DailyIncoming',
