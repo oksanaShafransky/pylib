ef711194de30db72f4fa9519fc3df2e28d98a3a8
Felix Vaisman <felixv@similargroup.com>
2016-01-19 19:57:46 +0200

    add histograms to desktop window as well

--- sw_airflow/dags/sw/desktop/desktop_moving_window.py
+++ sw_airflow/dags/sw/desktop/desktop_moving_window.py
@@ -875,11 +875,11 @@ def generate_dags(mode):
         DockerBashOperator(task_id='StoreSiteStatsTableSplits',
                            dag=dag,
                            docker_name='''{{ params.cluster }}''',
-                           bash_command='''hadoopexec {{ params.execution_dir }} mobile.jar com.similargroup.common.job.topvalues.KeyHistogramAnalysisUtil \
-                                           -in {{ params.base_hdfs_dir }}/{{ params.mode }}/histogram/type={{ params.mode_type }}/{{ macros.date_partition(ds) }}/sites-stat \
-                                           -d {{ macros.last_interval_day(ds, dag.schedule_interval) }} \
-                                           -k 2000000 \
-                                           -t app_sdk_stats{{ macros.hbase_table_suffix_partition(ds, params.mode, params.mode_type) \
+                           bash_command='''hadoopexec {{ params.execution_dir }} mobile.jar com.similargroup.common.job.topvalues.KeyHistogramAnalysisUtil
+                                           -in {{ params.base_hdfs_dir }}/{{ params.mode }}/histogram/type={{ params.mode_type }}/{{ macros.date_partition(ds) }}/sites-stat
+                                           -d {{ macros.last_interval_day(ds, dag.schedule_interval) }}
+                                           -k 500000
+                                           -t app_sdk_stats{{ macros.hbase_table_suffix_partition(ds, params.mode, params.mode_type)
                                         '''
                            )
     sites_stat_hist_register.set_upstream(sites_stat_hist_sensor)
@@ -896,11 +896,11 @@ def generate_dags(mode):
         DockerBashOperator(task_id='StoreSiteInfoTableSplits',
                            dag=dag,
                            docker_name='''{{ params.cluster }}''',
-                           bash_command='''hadoopexec {{ params.execution_dir }} mobile.jar com.similargroup.common.job.topvalues.KeyHistogramAnalysisUtil \
-                                           -in {{ params.base_hdfs_dir }}/{{ params.mode }}/histogram/type={{ params.mode_type }}/{{ macros.date_partition(ds) }}/sites-info \
-                                           -d {{ macros.last_interval_day(ds, dag.schedule_interval) }} \
-                                           -k 2000000 \
-                                           -t app_sdk_stats{{ macros.hbase_table_suffix_partition(ds, params.mode, params.mode_type) \
+                           bash_command='''hadoopexec {{ params.execution_dir }} mobile.jar com.similargroup.common.job.topvalues.KeyHistogramAnalysisUtil
+                                           -in {{ params.base_hdfs_dir }}/{{ params.mode }}/histogram/type={{ params.mode_type }}/{{ macros.date_partition(ds) }}/sites-info
+                                           -d {{ macros.last_interval_day(ds, dag.schedule_interval) }}
+                                           -k 500000
+                                           -t app_sdk_stats{{ macros.hbase_table_suffix_partition(ds, params.mode, params.mode_type)
                                         '''
                            )
     site_info_hist_register.set_upstream(site_info_hist_sensor)
--- sw_airflow/dags/sw/mobile/mobile_apps/moving_window.py
+++ sw_airflow/dags/sw/mobile/mobile_apps/moving_window.py
@@ -659,12 +659,12 @@ def generate_dag(mode):
         DockerBashOperator(task_id='StoreAppSdkTableSplits',
                            dag=dag,
                            docker_name='''{{ params.cluster }}''',
-                           bash_command='''hadoopexec {{ params.execution_dir }} mobile.jar com.similargroup.common.job.topvalues.KeyHistogramAnalysisUtil \
-                                           -in ({{ params.base_hdfs_dir }}/{{ params.mode }}/histogram/type={{ params.mode_type }}/{{ macros.date_partition(ds) }}/app-sdk-stats \
-                                           -d {{ macros.last_interval_day(ds, dag.schedule_interval) }} \
-                                           -k 500000 \
-                                           -t app_sdk_stats{{ macros.hbase_table_suffix_partition(ds, params.mode, params.mode_type) \
-                                         '''
+                           bash_command='''hadoopexec {{ params.execution_dir }} mobile.jar com.similargroup.common.job.topvalues.KeyHistogramAnalysisUtil
+                                           -in ({{ params.base_hdfs_dir }}/{{ params.mode }}/histogram/type={{ params.mode_type }}/{{ macros.date_partition(ds) }}/app-sdk-stats
+                                           -d {{ macros.last_interval_day(ds, dag.schedule_interval) }}
+                                           -k 500000
+                                           -t app_sdk_stats{{ macros.hbase_table_suffix_partition(ds, params.mode, params.mode_type)
+                                        '''
                            )
     app_sdk_hist_register.set_upstream(app_sdk_hist_sensor)
 
@@ -680,7 +680,12 @@ def generate_dag(mode):
         DockerBashOperator(task_id='StoreAppRanksTableSplits',
                            dag=dag,
                            docker_name='''{{ params.cluster }}''',
-                           bash_command='''hadoopexec {{ params.execution_dir }} mobile.jar com.similargroup.common.job.topvalues.KeyHistogramAnalysisUtil -in {{ params.base_hdfs_dir }}/{{ params.mode }}/histogram/type={{ params.mode_type }}/{{ macros.date_partition(ds) }}/app-eng-rank -d {{ macros.last_interval_day(ds, dag.schedule_interval) }} -k 500000 -t app_sdk_stats{{ macros.hbase_table_suffix_partition(ds, params.mode, params.mode_type)'''
+                           bash_command='''hadoopexec {{ params.execution_dir }} mobile.jar com.similargroup.common.job.topvalues.KeyHistogramAnalysisUtil
+                                           -in {{ params.base_hdfs_dir }}/{{ params.mode }}/histogram/type={{ params.mode_type }}/{{ macros.date_partition(ds) }}/app-eng-rank
+                                           -d {{ macros.last_interval_day(ds, dag.schedule_interval) }}
+                                           -k 500000
+                                           -t app_sdk_stats{{ macros.hbase_table_suffix_partition(ds, params.mode, params.mode_type) }}
+                                        '''
                            )
     app_eng_rank_hist_register.set_upstream(app_eng_rank_hist_sensor)
 
