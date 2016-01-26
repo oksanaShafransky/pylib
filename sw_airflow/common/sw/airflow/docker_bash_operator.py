32ca4f71afd571cc2bb0bf55103c9e484ea2f1d6
Barak Gitsis <barakg@similarweb.com>
2016-01-26 22:35:00 +0200

    stable DockerBashOperatorBuilder and mobile web estimation dag

--- sw_airflow/common/sw/airflow/docker_bash_operator.py
+++ sw_airflow/common/sw/airflow/docker_bash_operator.py
@@ -8,45 +8,120 @@ from airflow.utils import apply_defaults
 
 class DockerBashOperator(BashOperator):
     ui_color = '#FFFF66'
-    template_fields = ('bash_command', 'docker_name', 'kill_cmd')
-    cmd_template = '''docker -H=tcp://{{ params.docker_gate }}:2375 run       \
--v {{ params.execution_dir }}:/tmp/dockexec/%(random)s        \
--v /etc/localtime:/etc/localtime:ro                           \
--v /tmp/logs:/tmp/logs                                        \
--v /var/lib/sss:/var/lib/sss                                  \
--v /etc/localtime:/etc/localtime:ro                           \
--v /usr/bin:/opt/old_bin                                      \
--v /var/run/similargroup:/var/run/similargroup                \
---rm                                                          \
---sig-proxy=false                                             \
---user=`id -u`                                                \
--e DOCKER_GATE={{ docker_manager }}                           \
--e GELF_HOST="runsrv2.sg.internal"                            \
--e HOME=/tmp                                                  \
+    template_fields = ('bash_command', 'kill_cmd')
+    cmd_template = '''docker -H=tcp://{{ params.docker_gate }}:2375 run\
+-v {{ params.execution_dir }}:/tmp/dockexec/%(random)s \
+-v /etc/localtime:/etc/localtime:ro \
+-v /tmp/logs:/tmp/logs \
+-v /var/lib/sss:/var/lib/sss \
+-v /etc/localtime:/etc/localtime:ro \
+-v /usr/bin:/opt/old_bin \
+-v /var/run/similargroup:/var/run/similargroup \
+--rm \
+--sig-proxy=false \
+--user=`id -u` \
+-e DOCKER_GATE={{ docker_manager }} \
+-e GELF_HOST="runsrv2.sg.internal" \
+-e HOME=/tmp \
 runsrv/%(docker)s bash -c "sudo mkdir -p {{ params.execution_dir }} && sudo cp -r /tmp/dockexec/%(random)s/* {{ params.execution_dir }} && %(bash_command)s"
     '''
 
     kill_cmd_template = '''docker -H=tcp://{{ params.docker_gate }}:2375 rm -f %(container_name)s'''
-    kill_cmd = ''
 
     @apply_defaults
     def __init__(self, docker_name, bash_command, *args, **kwargs):
         super(DockerBashOperator, self).__init__(bash_command=None, *args, **kwargs)
 
-        rand = str(random.randint(10000, 99999))
-
         self.docker_name = docker_name
-        self.container_name = '''%(dag_id)s.%(task_id)s.%(rand)s''' % {'dag_id': self.dag.dag_id, 'task_id': self.task_id, 'rand': rand}
 
-        docker_command = DockerBashOperator.cmd_template % {'random': rand, 'container_name': self.container_name, 'docker': self.docker_name,
-                                                                     'bash_command': bash_command}
-
+        rand = str(random.randint(10000, 99999))
+        self.container_name = '''%(dag_id)s.%(task_id)s.%(rand)s''' % {'dag_id': self.dag.dag_id,
+                                                                       'task_id': self.task_id, 'rand': rand}
+        docker_command = DockerBashOperator.cmd_template % {'random': rand, 'container_name': self.container_name,
+                                                            'docker': self.docker_name,
+                                                            'bash_command': bash_command}
         self.kill_cmd = DockerBashOperator.kill_cmd_template % {'container_name': self.container_name}
         self.bash_command = docker_command
 
     def on_kill(self):
         logging.info('Killing container %s' % self.container_name)
-
         subprocess.call(['bash', '-c', self.kill_cmd])
-
         super(DockerBashOperator, self).on_kill()
+
+
+class DockerBashOperatorBuilder():
+    def __init__(self):
+        self.script_path = None
+        self.core_command = None
+        self.base_data_dir = None
+        self.docker_name = None
+        self.cmd_components = []
+        self.dag = None
+        self.date_template = None
+
+    def deepcopy(self, other):
+        other.script_path = self.script_path
+        other.core_command = self.core_command
+        other.base_data_dir = self.base_data_dir
+        other.docker_name = self.base_data_dir
+        other.cmd_components = self.cmd_components
+        other.dag = self.dag
+        other.date_template = self.date_template
+        return other
+
+    def set_docker_name(self, docker_name):
+        self.docker_name = docker_name
+        return self
+
+    def set_script_path(self, script_path):
+        self.script_path = script_path
+        return self
+
+    def set_core_command(self, core_command):
+        self.core_command = core_command
+        return self
+
+    def set_base_data_dir(self, base_data_dir):
+        self.base_data_dir = base_data_dir
+        return self
+
+    def set_dag(self, dag):
+        self.dag = dag
+        return self
+
+    def set_date_template(self, date_template):
+        self.date_template = date_template
+        return self
+
+    def add_cmd_component(self, cmd_component):
+        self.cmd_components.append(cmd_component)
+        return self
+
+    def reset_cmd_components(self):
+        self.cmd_components = []
+        return self
+
+    def build(self, task_id, core_command=None):
+        if core_command:
+            full_command = core_command
+        elif self.core_command:
+            full_command = self.core_command
+        else:
+            raise DockerBashOperatorBuilderException("Core bash command not set")
+
+        if self.script_path:
+            full_command = self.script_path + '/' + full_command
+
+        if self.date_template:
+            full_command += ' -d ' + self.date_template
+
+        full_command += ' -bd ' + self.base_data_dir
+        full_command += ' ' + ' '.join(self.cmd_components)
+
+        logging.info('Building %s.%s="%s"' % (self.dag.dag_id, task_id, full_command))
+        return DockerBashOperator(task_id=task_id, dag=self.dag, docker_name=self.docker_name,
+                                  bash_command=full_command)
+
+
+class DockerBashOperatorBuilderException(Exception):
+    pass
---
+++ sw_airflow/dags_dev/sw/mobile/web/estimation.py
@@ -0,0 +1,102 @@
+import copy
+
+__author__ = 'Barak Gitsis'
+
+from datetime import datetime, timedelta
+from airflow.models import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.sensors import ExternalTaskSensor
+
+from sw.airflow.key_value import *
+from sw.airflow.docker_bash_operator import DockerBashOperatorBuilder
+
+DEFAULT_EXECUTION_DIR = '/similargroup/production'
+SCRIPTS_DIR = DEFAULT_EXECUTION_DIR + '/mobile/scripts/web'
+BASE_DIR = '/similargroup/data/mobile-analytics'
+DOCKER_MANAGER = 'docker-a02.sg.internal'
+DEFAULT_CLUSTER = 'mrp'
+
+dag_args = {
+    'owner': 'similarweb',
+    'start_date': datetime(2016, 1, 24),
+    'depends_on_past': True,
+    'email': ['barakg@similarweb.com', 'amitr@similarweb.com>'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=15)
+}
+
+dag_template_params = {'execution_dir': DEFAULT_EXECUTION_DIR, 'code': SCRIPTS_DIR,
+                       'docker_gate': DOCKER_MANAGER,
+                       'data_dir': BASE_DIR, 'run_environment': 'PRODUCTION', 'cluster': DEFAULT_CLUSTER}
+
+dag = DAG(dag_id='Mobile_Web_Estimation', default_args=dag_args, params=dag_template_params, schedule_interval="@daily")
+dag.p = dag_template_params
+
+mobile_preliminary = ExternalTaskSensor(external_dag_id='Mobile_Preliminary',
+                                        dag=dag,
+                                        task_id="mobile_preliminary",
+                                        external_task_id='Preliminary')
+
+###################
+# Mobile Web Main #
+###################
+builder = DockerBashOperatorBuilder() \
+    .set_docker_name('''{{ params.cluster }}''') \
+    .set_script_path('''{{ params.code }}''') \
+    .set_dag(dag) \
+    .set_base_data_dir('''{{ params.data_dir }}''') \
+    .set_date_template('''{{ ds }}''') \
+    .add_cmd_component('-env main')
+
+main_sums = builder.build(task_id='main_sums', core_command='daily_est.sh -p source_sums')
+main_sums.set_upstream(mobile_preliminary)
+
+main_estimation = builder.build(task_id='main_estimation', core_command='daily_est.sh -p est')
+main_estimation.set_upstream(main_sums)
+
+main = DummyOperator(task_id='main', dag=dag)
+main.set_upstream(main_estimation)
+
+main_estimation_check = builder.build(task_id='main_estimation_check',
+                                      core_command='check_first_stage_estimates.sh')
+main_estimation_check.set_upstream(main_estimation)
+
+########################
+# Mobile Web Daily Cut #
+########################
+dc_builder = builder.deepcopy(DockerBashOperatorBuilder()).reset_cmd_components().add_cmd_component('-env daily-cut')
+
+daily_cut_sums = dc_builder.build(task_id='daily_cut_sums', core_command='daily_est.sh -p source_sums')
+daily_cut_sums.set_upstream(mobile_preliminary)
+
+daily_cut_estimation = dc_builder.build(task_id='daily_cut_estimation', core_command='daily_est.sh -p est')
+daily_cut_estimation.set_upstream(daily_cut_sums)
+
+daily_cut_estimation_check = dc_builder.build(task_id='daily_cut_estimation_check',
+                                              core_command='check_first_stage_estimates.sh')
+daily_cut_estimation_check.set_upstream(daily_cut_estimation)
+
+daily_cut_weights = dc_builder.build(task_id='daily_cut_weights', core_command='daily_est.sh -p weights')
+daily_cut_weights.set_upstream(daily_cut_estimation)
+
+daily_cut = DummyOperator(task_id='daily_cut', dag=dag)
+daily_cut.set_upstream(daily_cut_weights)
+
+daily_cut_weights_check = dc_builder.build(task_id='daily_cut_weights_check',
+                                           core_command='check_weight_calculations.sh')
+daily_cut_weights_check.set_upstream(daily_cut_weights)
+
+###########
+# Wrap-up #
+###########
+register_success = KeyValueSetOperator(task_id='register_success', dag=dag,
+                                       path='''services/mobile-web-daily-est/daily/{{ ds }}''',
+                                       env='DEV'
+                                       )
+register_success.set_upstream([daily_cut, main])
+
+mobile_web_estimation = DummyOperator(task_id='mobile_web_estimation', dag=dag)
+mobile_web_estimation.set_upstream(register_success)
+mobile_web_estimation.set_upstream([daily_cut, main])
