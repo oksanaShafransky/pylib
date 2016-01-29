07caed444769217228adcc6952980064c208e58d
iddo <iddo.aviram@similarweb.com>
2016-01-29 04:00:17 +0200

    Setting the starting date of the ios estimation DAG to November 1st

--- sw_airflow/dags/sw/mobile/ios_apps/estimation.py
+++ sw_airflow/dags/sw/mobile/ios_apps/estimation.py
@@ -17,7 +17,7 @@ ETCD_ENV_ROOT = {'STAGE': 'v1/dev', 'PRODUCTION': 'v1/production'}
 
 dag_args = {
     'owner': 'similarweb',
-    'start_date': datetime(2015, 12, 1),
+    'start_date': datetime(2015, 11, 1),
     'depends_on_past': False,
     'email': ['iddo.aviram@similarweb.com'],
     'email_on_failure': True,
