8cb4a5c356f3496bc3349f8c9e5542ca19e7eb57
Barak Gitsis <barakg@similarweb.com>
2016-02-01 20:26:31 +0200

    MW: moving_window - rename adjust_calc to adjust_calc_redist, formatting

--- sw_airflow/dags/sw/mobile/web/moving_window.py
+++ sw_airflow/dags/sw/mobile/web/moving_window.py
@@ -61,29 +61,24 @@ def assemble_process(mode, dag, sum_ww_value_size):
                                         script_path='''{{ params.execution_dir }}/mobile/scripts/web''',
                                         additional_cmd_components=['-env main'])
 
-    prepare_hbase_tables = \
-        factory.build(task_id='prepare_hbase_tables',
-                      core_command='../start-process.sh -p tables -fl MOBILE_WEB')
+    prepare_hbase_tables = factory.build(task_id='prepare_hbase_tables',
+                                         core_command='../start-process.sh -p tables -fl MOBILE_WEB')
     prepare_hbase_tables.set_upstream(should_run_mw)
 
-    popular_pages_agg = \
-        factory.build(task_id='popular_pages_agg',
-                      core_command='popular_pages.sh -p aggregate_popular_pages')
+    popular_pages_agg = factory.build(task_id='popular_pages_agg',
+                                      core_command='popular_pages.sh -p aggregate_popular_pages')
     popular_pages_agg.set_upstream(should_run_mw)
 
-    popular_pages_top_store = \
-        factory.build(task_id='popular_pages_top_store',
-                      core_command='popular_pages.sh -p top_popular_pages')
+    popular_pages_top_store = factory.build(task_id='popular_pages_top_store',
+                                            core_command='popular_pages.sh -p top_popular_pages')
     popular_pages_top_store.set_upstream([prepare_hbase_tables, popular_pages_agg])
 
-    gaps_filler = \
-        factory.build(task_id='gaps_filler',
-                      core_command='mobile_web_gaps_filler.sh')
+    gaps_filler = factory.build(task_id='gaps_filler',
+                                core_command='mobile_web_gaps_filler.sh')
     gaps_filler.set_upstream(should_run_mw)
 
-    first_stage_agg = \
-        factory.build(task_id='first_stage_agg',
-                      core_command='first_stage_agg.sh')
+    first_stage_agg = factory.build(task_id='first_stage_agg',
+                                    core_command='first_stage_agg.sh')
     first_stage_agg.set_upstream(should_run_mw)
 
     # ############################################################################################################
@@ -94,26 +89,22 @@ def assemble_process(mode, dag, sum_ww_value_size):
     new_algo_factory.mode_type = 'last-28'
     new_algo_factory.add_cmd_component('-wenv daily-cut')
 
-    adjust_calc_intermediate = \
-        new_algo_factory.build(task_id='adjust_calc_intermediate',
-                               core_command='adjust_est.sh -p prepare_data,predict')
+    adjust_calc_intermediate = new_algo_factory.build(task_id='adjust_calc_intermediate',
+                                                      core_command='adjust_est.sh -p prepare_data,predict')
     adjust_calc_intermediate.set_upstream(first_stage_agg)
 
-    adjust_calc = \
-        new_algo_factory.build(task_id='adjust_calc',
-                               core_command='adjust_est.sh -p redist')
-    adjust_calc.set_upstream([gaps_filler, adjust_calc_intermediate])
+    adjust_calc_redist = new_algo_factory.build(task_id='adjust_calc_redist',
+                                                core_command='adjust_est.sh -p redist')
+    adjust_calc_redist.set_upstream([gaps_filler, adjust_calc_intermediate])
     # ###########################################################################################################
 
-    check_daily_estimations = \
-        factory.build(task_id='check_daily_estimations',
-                      core_command='check_daily_estimations.sh')
-    check_daily_estimations.set_upstream(adjust_calc)
+    check_daily_estimations = factory.build(task_id='check_daily_estimations',
+                                            core_command='check_daily_estimations.sh')
+    check_daily_estimations.set_upstream(adjust_calc_redist)
 
-    calc_subdomains = \
-        factory.build(task_id='calc_subdomains',
-                      core_command='calc_subdomains.sh')
-    calc_subdomains.set_upstream([adjust_calc, prepare_hbase_tables])
+    calc_subdomains = factory.build(task_id='calc_subdomains',
+                                    core_command='calc_subdomains.sh')
+    calc_subdomains.set_upstream([adjust_calc_redist, prepare_hbase_tables])
 
     # ww value aggregation
     sum_ww_factory = copy.copy(factory)
@@ -123,13 +114,12 @@ def assemble_process(mode, dag, sum_ww_value_size):
     sum_ww_all = []
     for i in range(0, sum_ww_value_size):
         sum_ww_i = sum_ww_factory.build(task_id='sum_ww_day_%s' % i, core_command='sum_ww.sh')
-        sum_ww_i.set_upstream(adjust_calc)
+        sum_ww_i.set_upstream(adjust_calc_redist)
         sum_ww_all.append(sum_ww_i)
 
     # load to Hbase
-    adjust_store = \
-        factory.build(task_id='adjust_store',
-                      core_command='adjust_est.sh -p store -ww')
+    adjust_store = factory.build(task_id='adjust_store',
+                                 core_command='adjust_est.sh -p store -ww')
     adjust_store.set_upstream([prepare_hbase_tables] + sum_ww_all)
 
     # FINISH WINDOW PART
@@ -146,10 +136,10 @@ def assemble_process(mode, dag, sum_ww_value_size):
             factory.build(task_id='predict_validate',
                           core_command='second_stage_tests.sh -wenv daily-cut -p prepare_predictions_for_test,verify_predictions'
                           )
-        predict_validate.set_upstream([predict_validate_preparation, adjust_calc])
+        predict_validate.set_upstream([predict_validate_preparation, adjust_calc_redist])
 
-        compare_est_to_qc = \
-            factory.build(task_id='compare_est_to_qc', core_command='compare_estimations_to_qc.sh -sm')
+        compare_est_to_qc = factory.build(task_id='compare_est_to_qc',
+                                          core_command='compare_estimations_to_qc.sh -sm')
         compare_est_to_qc.set_upstream(first_stage_agg)
 
         mobile_web.set_upstream([predict_validate, compare_est_to_qc])
