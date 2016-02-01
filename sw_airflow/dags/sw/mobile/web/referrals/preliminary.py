f8f07d328aea5180412cdddd69157003d98bd0cf
Barak Gitsis <barakg@similarweb.com>
2016-02-01 20:25:08 +0200

    MW: refactor referrals preliminary to use factory

--- sw_airflow/dags/sw/mobile/web/referrals/preliminary.py
+++ sw_airflow/dags/sw/mobile/web/referrals/preliminary.py
@@ -1,12 +1,9 @@
-__author__ = 'Amit Rom'
-
-from datetime import datetime, timedelta
-
 from airflow.models import DAG
 from airflow.operators.dummy_operator import DummyOperator
+from datetime import timedelta
 
 from sw.airflow.airflow_etcd import *
-from sw.airflow.operators import DockerBashOperator
+from sw.airflow.docker_bash_operator import DockerBashOperatorFactory
 
 DEFAULT_EXECUTION_DIR = '/similargroup/production'
 BASE_DIR = '/similargroup/data/mobile-analytics'
@@ -19,7 +16,7 @@ dag_args = {
     'owner': 'similarweb',
     'start_date': datetime(2015, 12, 1),
     'depends_on_past': True,
-    'email': ['amitr@similarweb.com'],
+    'email': ['amitr@similarweb.com,barakg@similarweb.com'],
     'email_on_failure': True,
     'email_on_retry': False,
     'retries': 8,
@@ -29,38 +26,31 @@ dag_args = {
 dag_template_params = {'execution_dir': DEFAULT_EXECUTION_DIR, 'docker_gate': DOCKER_MANAGER,
                        'base_hdfs_dir': BASE_DIR, 'run_environment': 'PRODUCTION', 'cluster': DEFAULT_CLUSTER}
 
-dag = DAG(dag_id='MobileWebReferralsDailyPreliminary', default_args=dag_args, params=dag_template_params, schedule_interval=timedelta(days=1))
+dag = DAG(dag_id='MobileWeb_ReferralsPreliminary', default_args=dag_args, params=dag_template_params,
+          schedule_interval=timedelta(days=1))
 
-
-opera_raw_data_ready = EtcdSensor(task_id='OperaRawDataReady',
+opera_raw_data_ready = EtcdSensor(task_id='opera_raw_data_ready',
                                   dag=dag,
                                   root=ETCD_ENV_ROOT[dag_template_params['run_environment']],
                                   path='''services/opera-mini-s3/daily/{{ ds }}'''
                                   )
 
+factory = DockerBashOperatorFactory(use_defaults=True,
+                                    dag=dag,
+                                    script_path='''{{ params.execution_dir }}/mobile/scripts/web/referrals''',
+                                    additional_cmd_components=['-env main'])
 
-filter_malformed_events = DockerBashOperator(task_id='FilterMalformedEvents',
-                                             dag=dag,
-                                             docker_name='''{{ params.cluster }}''',
-                                             bash_command='''{{ params.execution_dir }}/mobile/scripts/web/referrals/preliminary.sh -d {{ ds }} -p filter_malformed_events -env main'''
-                                             )
+filter_malformed_events = factory.build(task_id='filter_malformed_events',
+                                        core_command='preliminary.sh -p filter_malformed_events')
 filter_malformed_events.set_upstream(opera_raw_data_ready)
 
-
-extract_invalid_users = DockerBashOperator(task_id='ExtractInvalidUsers',
-                                           dag=dag,
-                                           docker_name=DEFAULT_CLUSTER,
-                                           bash_command='''{{ params.execution_dir }}/mobile/scripts/web/referrals/preliminary.sh -d {{ ds }} -p filter_users -env main'''
-                                           )
+extract_invalid_users = factory.build(task_id='extract_invalid_users',
+                                      core_command='preliminary.sh -p filter_users ')
 extract_invalid_users.set_upstream(filter_malformed_events)
 
-
-filter_invalid_users = DockerBashOperator(task_id='FilterInvalidUsers',
-                                          dag=dag,
-                                          docker_name=DEFAULT_CLUSTER,
-                                          bash_command='''{{ params.execution_dir }}/mobile/scripts/web/referrals/preliminary.sh -d {{ ds }} -p filter_invalid_users_from_events -env main'''
-                                          )
+filter_invalid_users = factory.build(task_id='filter_invalid_users',
+                                     core_command='preliminary.sh -p filter_invalid_users_from_events''')
 filter_invalid_users.set_upstream(extract_invalid_users)
 
-mobile_web_referrals_preliminary = DummyOperator(task_id='MobileWebReferralsDailyPreliminary', dag=dag)
-mobile_web_referrals_preliminary.set_upstream(filter_invalid_users)
+referrals_preliminary = DummyOperator(task_id='ReferralsPreliminary', dag=dag)
+referrals_preliminary.set_upstream(filter_invalid_users)
