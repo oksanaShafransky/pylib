0aa48af7270f326425e2a29f3b1689cef759a707
Felix Vaisman <felixv@similargroup.com>
2016-02-02 11:23:37 +0200

    conditional stage cleanup

--- sw_airflow/dags/sw/mobile/android_apps/moving_window.py
+++ sw_airflow/dags/sw/mobile/android_apps/moving_window.py
@@ -502,7 +502,7 @@ def generate_dag(mode):
                                        dag=dag)
 
     # for now, skip cleanup
-    should_clean_stage = BranchPythonOperator(lambda x: skip_clean_id)
+    should_clean_stage = BranchPythonOperator(python_callable=lambda x: skip_clean_id)
     should_clean_stage.set_downstream([cleaning_stage, not_cleaning_stage])
 
     ############################
