51c137d0028d35006097e96f523274c3b4f0a45f
Felix Vaisman <felixv@similargroup.com>
2016-02-02 11:18:19 +0200

    conditional stage cleanup

--- sw_airflow/dags/sw/mobile/android_apps/moving_window.py
+++ sw_airflow/dags/sw/mobile/android_apps/moving_window.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 from airflow.models import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.sensors import HdfsSensor
+from airflow.operators.python_operator import BranchPythonOperator
 from sw.airflow.key_value import *
 from sw.airflow.operators import DockerBashOperator
 from sw.airflow.operators import  DockerCopyHbaseTableOperator
@@ -476,6 +477,10 @@ def generate_dag(mode):
 
     if is_window_dag():
 
+        clean_id = 'StageCleanupRequested'
+        cleaning_stage = DummyOperator(task_id=clean_id,
+                                       dag=dag)
+
         cleanup_stage = DummyOperator(task_id='CleanupStage',
                                       dag=dag
                                       )
@@ -485,10 +490,20 @@ def generate_dag(mode):
                 DockerBashOperator(task_id='CleanupStage_DS-%s' % i,
                                    dag=dag,
                                    docker_name='''{{ params.cluster }}''',
-                                   bash_command='''{{ params.execution_dir }}/mobile/scripts/windowCleanup.sh -d {{ macros.ds_add(ds,-%s) }} -bd {{ params.base_hdfs_dir }} -m {{ params.mode }} -mt {{ params.mode_type }} -fl apps -p delete_files -p drop_hbase_tables''' % i
+                                   bash_command='''{{ params.execution_dir }}/mobile/scripts/windowCleanup.sh -d {{ macros.ds_add(ds,-%s) }} -bd {{ params.base_hdfs_dir }} -m {{ params.mode }} -mt {{ params.mode_type }} -fl apps -p delete_files -p drop_hbase_tables''' % i,
+
                                    )
             cleanup_stage_ds_minus_i.set_upstream(apps)
+            cleanup_stage_ds_minus_i.set_upstream(cleaning_stage)
             cleanup_stage.set_upstream(cleanup_stage_ds_minus_i)
+
+    skip_clean_id = 'StageCleanupSkipped'
+    not_cleaning_stage = DummyOperator(task_id=skip_clean_id,
+                                       dag=dag)
+
+    # for now, skip cleanup
+    should_clean_stage = BranchPythonOperator(lambda x: skip_clean_id)
+    should_clean_stage.set_downstream([cleaning_stage, not_cleaning_stage])
 
     ############################
     # Local Availability Dates #
