7a35d1bfe4111fcf0e2b53bf6b80ee9c3a24620c
Barak Gitsis <barakg@similarweb.com>
2016-02-16 22:52:20 +0200

    MW: split out daily from moving_window

--- sw_airflow/dags/sw/mobile/web/moving_window.py
+++ sw_airflow/dags/sw/mobile/web/moving_window.py
@@ -42,18 +42,7 @@ window_dag = DAG(dag_id='MobileWeb_Window', default_args=dag_args, params=window
                  schedule_interval='@daily')
 
 
-def assemble_process(mode, dag, sum_ww_value_size):
-    estimation = AdaptedExternalTaskSensor(external_dag_id='MobileWeb_Estimation', dag=dag,
-                                           task_id="MobileWeb_Estimation",
-                                           external_task_id='Estimation')
-    desktop_estimation_aggregation = AdaptedExternalTaskSensor(external_dag_id='Desktop_DailyEstimation',
-                                                               dag=dag,
-                                                               task_id='MonthlySumEstimationParameters',
-                                                               external_task_id='SumEstimation')
-
-    should_run_mw = DummyOperator(dag=dag, task_id='should_run_mw')
-    should_run_mw.set_upstream([estimation, desktop_estimation_aggregation])
-
+def assemble_process(mode, dag):
     factory = DockerBashOperatorFactory(use_defaults=True,
                                         dag=dag,
                                         script_path='''{{ params.execution_dir }}/mobile/scripts/web''',
@@ -62,88 +51,59 @@ def assemble_process(mode, dag, sum_ww_value_size):
     prepare_hbase_tables = factory.build(task_id='prepare_hbase_tables',
                                          core_command='../start-process.sh -p tables -fl MOBILE_WEB')
 
-    # ############################################################################################################
-    # LOGIC
+    adjust_store = add_sum_ww(dag, factory, prepare_hbase_tables)
 
     popular_pages_top_store = add_popular_pages(dag, factory, prepare_hbase_tables)
+    daily_redist = AdaptedExternalTaskSensor(external_dag_id='MobileWeb_Daily', dag=dag,
+                                             task_id="MobileWeb_Daily_redist",
+                                             external_task_id='redist')
+    calc_subdomains = add_calc_subdomains(daily_redist, factory, prepare_hbase_tables)
 
-    gaps_filler = factory.build(task_id='gaps_filler',
-                                core_command='airflow_mobile_web_gaps_filler.sh')
-    gaps_filler.set_upstream(should_run_mw)
-
-    # ##############################################################
-    # the reason for new factory here is that we control whether to use new
-    # algo through mode and mode type params
-
-    new_algo_factory = copy.copy(factory)
-    new_algo_factory.mode = 'window'
-    new_algo_factory.mode_type = 'last-28'
-
-    first_stage_agg = new_algo_factory.build(task_id='first_stage_agg',
-                                             core_command='first_stage_agg.sh')
-    first_stage_agg.set_upstream(should_run_mw)
-
-    predict_for_day = \
-        new_algo_factory.build(task_id='predict_for_day',
-                               core_command='adjust_est.sh -p prepare_data,predict -wenv daily-cut')
-    predict_for_day.set_upstream(first_stage_agg)
-
-    adjust_calc_redist = new_algo_factory.build(task_id='adjust_calc_redist',
-                                                core_command='adjust_est.sh -p redist -wenv daily-cut')
-    adjust_calc_redist.set_upstream([gaps_filler, predict_for_day])
-    # ###########################################################################################################
-
-    check_daily_estimations = factory.build(task_id='check_daily_estimations',
-                                            core_command='check_daily_estimations.sh')
-    check_daily_estimations.set_upstream(adjust_calc_redist)
-
-    calc_subdomains = factory.build(task_id='calc_subdomains',
-                                    core_command='calc_subdomains.sh')
-    calc_subdomains.set_upstream([adjust_calc_redist, prepare_hbase_tables])
-
-    # ww value aggregation
-    sum_ww_factory = copy.copy(factory)
-    sum_ww_factory.mode = 'daily'
-
-    sum_ww_all = []
-    for day_to_calculate in range(0, sum_ww_value_size):
-        sum_ww_i = sum_ww_factory.build(task_id='sum_ww_day_%s' % day_to_calculate,
-                                        date_template='''{{ macros.ds_add(ds,-%d) }}''' % day_to_calculate,
-                                        core_command='sum_ww.sh')
-        sum_ww_i.set_upstream(adjust_calc_redist)
-        sum_ww_all.append(sum_ww_i)
-
-    # load to Hbase
-    adjust_store = factory.build(task_id='adjust_store',
-                                 core_command='adjust_est.sh -p store -ww')
-    adjust_store.set_upstream([prepare_hbase_tables] + sum_ww_all)
-
-    # FINISH WINDOW PART
-    mobile_web = DummyOperator(task_id=dag.dag_id, dag=dag)
+    mobile_web = DummyOperator(task_id=dag.dag_id, dag=dag, sla=timedelta(days=1))
     mobile_web.set_upstream([adjust_store, calc_subdomains, popular_pages_top_store])
 
     if mode == SNAPSHOT_MODE:
         predict_validate_preparation = \
             factory.build(task_id='predict_validate_preparation',
                           core_command='second_stage_tests.sh -wenv daily-cut -p prepare_total_device_count')
-        predict_validate_preparation.set_upstream(should_run_mw)
 
         predict_validate = \
             factory.build(task_id='predict_validate',
                           core_command='second_stage_tests.sh -wenv daily-cut '
-                                       '-p prepare_predictions_for_test,verify_predictions'
-                          )
-        predict_validate.set_upstream([predict_validate_preparation, adjust_calc_redist])
+                                       '-p prepare_predictions_for_test,verify_predictions')
+        predict_validate.set_upstream([predict_validate_preparation, daily_redist])
 
+        first_stage_agg = AdaptedExternalTaskSensor(external_dag_id='MobileWeb_Daily', dag=dag,
+                                                    task_id="MobileWeb_Daily_first_stage_agg",
+                                                    external_task_id='first_stage_agg')
         compare_est_to_qc = factory.build(task_id='compare_est_to_qc',
                                           core_command='compare_estimations_to_qc.sh -sm')
         compare_est_to_qc.set_upstream(first_stage_agg)
 
+        estimation = AdaptedExternalTaskSensor(external_dag_id='MobileWeb_Estimation', dag=dag,
+                                               task_id="MobileWeb_Estimation",
+                                               external_task_id='Estimation')
+
         first_stage_agg_for_model = factory.build(task_id='first_stage_agg_for_model',
                                                   core_command='first_stage_agg.sh')
-        first_stage_agg_for_model.set_upstream(should_run_mw)
+        first_stage_agg_for_model.set_upstream(estimation)
 
         mobile_web.set_upstream([predict_validate, compare_est_to_qc, first_stage_agg_for_model])
+
+
+def add_calc_subdomains(daily_redist, factory, prepare_hbase_tables):
+    calc_subdomains = factory.build(task_id='calc_subdomains', core_command='calc_subdomains.sh')
+    calc_subdomains.set_upstream([daily_redist, prepare_hbase_tables])
+    return calc_subdomains
+
+
+def add_sum_ww(dag, factory, prepare_hbase_tables):
+    daily_sum_ww = AdaptedExternalTaskSensor(external_dag_id='MobileWeb_Daily', dag=dag,
+                                             task_id="MobileWeb_Daily_sum_ww",
+                                             external_task_id='sum_ww')
+    adjust_store = factory.build(task_id='adjust_store', core_command='adjust_est.sh -p store -ww')
+    adjust_store.set_upstream([daily_sum_ww, prepare_hbase_tables])
+    return adjust_store
 
 
 def add_popular_pages(dag, factory, prepare_hbase_tables):
@@ -168,5 +128,5 @@ def add_popular_pages(dag, factory, prepare_hbase_tables):
     return popular_pages_top_store
 
 
-assemble_process(SNAPSHOT_MODE, snapshot_dag, sum_ww_value_size=31)
-assemble_process(WINDOW_MODE, window_dag, sum_ww_value_size=int(WINDOW_MODE_TYPE.split('-')[1]))
+assemble_process(SNAPSHOT_MODE, snapshot_dag)
+assemble_process(WINDOW_MODE, window_dag)
