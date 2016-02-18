efbccb6a3f0a333735cb9bff650fac0b40b432c6
jonathan lavon <jonathan@similargroup.com>
2016-02-18 09:22:44 +0200

    Added ga export DAG

---
+++ sw_airflow/dags/sw/mobile/export_ga.py
@@ -0,0 +1,41 @@
+__author__ = 'lajonat'
+
+from datetime import datetime, timedelta
+
+from airflow.models import DAG
+
+from sw.airflow.docker_bash_operator import DockerBashOperator
+
+DEFAULT_EXECUTION_DIR = '/similargroup/production'
+BASE_DIR = '/similargroup/data'
+DOCKER_MANAGER = 'docker-a02.sg.internal'
+DEFAULT_CLUSTER = 'mrp'
+
+dag_args = {
+    'owner': 'similarweb',
+    'start_date': datetime(2016, 2, 18),
+    'depends_on_past': False,
+    'email': ['bigdata@similarweb.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 8,
+    'retry_delay': timedelta(minutes=15)
+}
+
+dag_template_params = {'execution_dir': DEFAULT_EXECUTION_DIR, 'docker_gate': DOCKER_MANAGER,
+                       'base_hdfs_dir': BASE_DIR, 'run_environment': 'PRODUCTION', 'cluster': DEFAULT_CLUSTER}
+
+dag = DAG(dag_id='Mobile_ExportGAData', default_args=dag_args, params=dag_template_params, schedule_interval="@daily")
+
+
+# define stages
+
+
+upload = DockerBashOperator(task_id='ExportData',
+                            dag=dag,
+                            docker_name='''{{ params.cluster }}''',
+                            bash_command='''{{ params.execution_dir }}/analytics/scripts/daily/export_ga_data.sh -d {{ ds }}'''
+                            )
+
+
+
