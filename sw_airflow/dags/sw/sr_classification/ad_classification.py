ac2277b2a88278af56e621dfa1bc53c4659f261e
Barak Gitsis <barakg@similarweb.com>
2016-02-25 21:32:38 +0200

    better dag example, hql separate files

--- sw_airflow/dags/sw/sr_classification/ad_classification.py
+++ sw_airflow/dags/sw/sr_classification/ad_classification.py
@@ -3,6 +3,7 @@ from airflow.operators.dummy_operator import DummyOperator
 from datetime import timedelta, datetime
 
 from sw.airflow.docker_bash_operator import DockerBashOperatorFactory
+from sw.airflow.external_sensors import AdaptedExternalTaskSensor
 
 dag_args = {
     'owner': 'SpecialReferrers',
@@ -31,8 +32,32 @@ factory = DockerBashOperatorFactory(
         base_data_dir='''{{ params.base_data_dir }}''',
         script_path='''{{ params.execution_dir }}/analytics/scripts/monthly/adclassification''',
 )
+desktop_window = AdaptedExternalTaskSensor(external_dag_id='Desktop_MovingWindow_Window',
+                                           dag=dag,
+                                           task_id='desktop_window',
+                                           external_task_id='MovingWindow_Window',
+                                           timeout=60 * 60 * 24)
 
-ad_classification = factory.build(task_id='ad_classification', core_command='adclassification.sh')
+stage1_1 = factory.build(task_id='stage1_1', core_command='adclassification.sh -s stage1')
+stage1_1.set_upstream(desktop_window)
 
-ad_classification_complete = DummyOperator(task_id='ad_classification_complete', dag=dag)
-ad_classification_complete.set_upstream([ad_classification])
+stage1_2 = factory.build(task_id='stage1_2', core_command='adclassification.sh -s stage2')
+stage1_2.set_upstream(desktop_window)
+
+stage1_3 = factory.build(task_id='stage1_3', core_command='adclassification.sh -s stage1')
+stage1_3.set_upstream(desktop_window)
+
+stage1_4 = factory.build(task_id='stage1_4', core_command='adclassification.sh -s stage2')
+stage1_4.set_upstream(desktop_window)
+stage1_tasks = [stage1_1, stage1_2, stage1_3, stage1_4]
+
+stage1_complete = DummyOperator(task_id='stage1_complete', dag=dag)
+stage1_complete.set_upstream(stage1_tasks)
+
+stage2_1 = factory.build(task_id='stage2_1', core_command='adclassification.sh -s stage2')
+stage2_1.set_upstream(stage1_complete)
+stage2_2 = factory.build(task_id='stage2_2', core_command='adclassification.sh -s stage2')
+stage2_2.set_upstream(stage1_complete)
+
+ad_classification_complete = DummyOperator(task_id='ad_classification_complete', dag=dag, sla=timedelta(days=2))
+ad_classification_complete.set_upstream([stage2_1, stage2_2])
