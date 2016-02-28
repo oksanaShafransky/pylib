c251dbc02e3fa5b0dc4e1974c35748bfa1387215
jonathan lavon <jonathan@similargroup.com>
2016-02-28 17:32:23 +0200

    SIM-8513 - Added itunes epf process to airflow

---
+++ sw_airflow/dags/sw/scraper/get_itunes_epf.py
@@ -0,0 +1,45 @@
+__author__ = 'lajonat'
+
+from datetime import datetime, timedelta
+
+from airflow.models import DAG
+
+from sw.airflow.docker_bash_operator import DockerBashOperator
+
+DEFAULT_EXECUTION_DIR = '/similargroup/production'
+BASE_DIR = '/similargroup/data'
+DOCKER_MANAGER = 'docker-a02.sg.internal'
+DEFAULT_CLUSTER = 'mrp'
+
+dag_args = {
+    'owner': 'similarweb',
+    'start_date': datetime(2016, 2, 28),
+    'depends_on_past': False,
+    'email': ['spiders@similarweb.com', 'n7i6d2a2m1h2l3f6@similar.slack.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 4,
+    'retry_delay': timedelta(minutes=15)
+}
+
+dag_template_params = {'execution_dir': DEFAULT_EXECUTION_DIR, 'docker_gate': DOCKER_MANAGER,
+                       'base_hdfs_dir': BASE_DIR, 'run_environment': 'PRODUCTION', 'cluster': DEFAULT_CLUSTER}
+
+dag = DAG(dag_id='Scraping_GetItunesEPF', default_args=dag_args, params=dag_template_params, schedule_interval="50 19 * * *")
+
+
+# define stages
+
+
+getepf = DockerBashOperator(task_id='GetEPF',
+                            dag=dag,
+                            docker_name='''{{ params.cluster }}''',
+                            bash_command='''{{ params.execution_dir }}/scraper/scripts/runepf.sh {{ macros.ds_format(macros.ds_add(ds, -1), '%Y-%m-%d', '%Y%m%d' }} -f'''
+                            )
+
+process = DockerBashOperator(task_id='ProcessEPF',
+                            dag=dag,
+                            docker_name='''{{ params.cluster }}''',
+                            bash_command='''{{ params.execution_dir }}/scraper/scripts/processepf.sh {{ macros.ds_format(macros.ds_add(ds, -1), '%Y-%m-%d', '%Y%m%d' }} -f'''
+)
+process.set_upstream(getepf)
