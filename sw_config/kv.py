03c83fe34a378cdab82b7a52fff0a1683ddd45cc
Felix Vaisman <felixv@similargroup.com>
2016-03-14 15:42:34 +0200

    minor fix for target env type

--- sw_config/kv.py
+++ sw_config/kv.py
@@ -18,6 +18,3 @@ class KeyValueProxy:
     # immediate sub keys only, with relative names
     def sub_keys(self, key):
         pass
-
-    def with_set(self, modified_key, modified_value):
-        self.get = lambda key: modified_value if key == modified_key else self.get
