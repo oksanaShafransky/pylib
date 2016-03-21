4ad26cbf5456f1b7a6207ae797718719d2b8ad23
kfire <kfire@similarweb.com>
2016-03-21 15:01:53 +0200

    change start date of AT moving window-window dag

--- sw_airflow/dags/sw/desktop/advanced_moving_window.py
+++ sw_airflow/dags/sw/desktop/advanced_moving_window.py
@@ -60,7 +60,7 @@ def generate_dags(mode):
 
     dag_args_for_mode = dag_args.copy()
     if is_window_dag():
-        dag_args_for_mode.update({'start_date': datetime(2016, 3, 1)})
+        dag_args_for_mode.update({'start_date': datetime(2016, 3, 20)})
 
     if is_snapshot_dag():
         dag_args_for_mode.update({'start_date': datetime(2016, 2, 1)})
