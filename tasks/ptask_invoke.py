a9c1592dc0cda71c84cf686c74b2be0d118dbf67
iddoa <iddo.aviram@similarweb.com>
2016-04-05 18:12:21 +0300

    Enhancement: regularizing names to use the ptask term

--- tasks/ptask_invoke.py
+++ tasks/ptask_invoke.py
@@ -3,7 +3,7 @@ from invoke import Program, Argument, Config
 from invoke.config import *
 import datetime
 
-class TaskConfig(Config):
+class PtaskConfig(Config):
     @staticmethod
     def global_defaults():
         global_defaults = Config.global_defaults()
@@ -14,9 +14,9 @@ class TaskConfig(Config):
         return global_defaults
 
 
-class TasksInvoker(Program):
+class PtaskInvoker(Program):
     def core_args(self):
-        core_args = super(TasksInvoker, self).core_args()
+        core_args = super(PtaskInvoker, self).core_args()
         extra_args = [
             Argument(names=('date','dt'), help="The task's logical day in ISO yyyy-MM-dd format", optional=True),
             Argument(names=('base_dir', 'bd'), help="The HDFS base directory for the task's output", optional=True),
@@ -27,11 +27,11 @@ class TasksInvoker(Program):
 
     @property
     def config(self):
-        config = super(TasksInvoker, self).config
+        config = super(PtaskInvoker, self).config
 
         sw_tasks = {}
         if self.args.date.value:
-            sw_tasks['date'] = TasksInvoker.__parse_date(self.args.date.value)
+            sw_tasks['date'] = PtaskInvoker.__parse_date(self.args.date.value)
         if self.args.base_dir.value:
             sw_tasks['base_dir'] = self.args.base_dir.value
         if self.args.dont_force.value:
@@ -45,7 +45,7 @@ class TasksInvoker(Program):
 
 
 def main():
-    program = TasksInvoker(config_class=TaskConfig)
+    program = PtaskInvoker(config_class=PtaskConfig)
     program.run()
 
 
