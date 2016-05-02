237cb9035fedb90394a987de6bd1507647c604e1
Barak Gitsis <barakg@similarweb.com>
2016-05-02 19:10:19 +0300

    create temp table logging

--- hive/common.py
+++ hive/common.py
@@ -209,7 +209,7 @@ def deploy_jar(deploy_path, jar_hdfs_location):
     subprocess.call(["hadoop", "fs", "-mkdir", "-p", jar_hdfs_location])
     subprocess.call(["hadoop", "fs", "-put", deploy_path + "/analytics.jar", jar_hdfs_location + "/analytics.jar"])
     subprocess.call(
-            ["hadoop", "fs", "-put", deploy_path + "/lib/common-1.0.jar", jar_hdfs_location + "/common.jar"])
+        ["hadoop", "fs", "-put", deploy_path + "/lib/common-1.0.jar", jar_hdfs_location + "/common.jar"])
 
 
 # use ['add jar %s' % jar for jar in detect_jars(...) ]
@@ -250,7 +250,7 @@ def table_location(table):
     for line in output.split("\n"):
         if "Location:" in line:
             return line.split("\t")[1]
-    raise Exception('Cannot find the location for table %s stdout[%s] stderr[%s]')
+    raise Exception('Cannot find the location for table %s stdout[%s] stderr[%s]' % (table, output, err))
 
 
 def hbase_table_name(table):
@@ -314,7 +314,7 @@ def should_create_external_hbase_table(orig_table_name, hbase_table_name_val):
 def temp_hbase_table_cmds(orig_table_name, hbase_root_table_name, mode, mode_type, date):
     full_hbase_table_name = hbase_root_table_name + hbase_table_suffix(date, mode, mode_type)
     logger.info(
-            "Checking whether to create external table %s for HBase table %s:" % (
+        "Checking whether to create external table %s for HBase table %s:" % (
             orig_table_name, full_hbase_table_name))
     if should_create_external_hbase_table(orig_table_name, full_hbase_table_name):
         logger.info("Writing to an external table with the given name.")
@@ -396,6 +396,7 @@ def list_days(end_date, mode, mode_type):
         return None
 
     return [end_date - timedelta(days=x) for x in range(0, delta.days)]
+
 
 def hbase_table_suffix(date, mode, mode_type, in_date_fmt='%Y-%m-%d'):
     # in_date_fmt='%Y-%m' if mode == 'snapshot' else '%Y-%m-%d'
