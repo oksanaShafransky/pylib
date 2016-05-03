c3b14d437ba40cdd833aa61f696182f192b90a09
amitr <amitr@similargroup.com>
2016-05-03 21:23:05 +0300

    SIM-9930: Kill Zombie Jobs Kill multiple zombies, if exist

--- jobs/kill_zombie_jobs.py
+++ jobs/kill_zombie_jobs.py
@@ -18,29 +18,30 @@ job_rm_port = 8088
 valid_states = 'SUBMITTED,ACCEPTED,RUNNING'
 user = 'airflow'
 
-zombie_apps_template = '%(server)s:%(port)d/ws/v1/cluster/apps?user=%(user)s&states=%(states)s&limit=1&applicationTags=%(task_id)s'
+zombie_apps_template = '%(server)s:%(port)d/ws/v1/cluster/apps?user=%(user)s&states=%(states)s&applicationTags=%(task_id)s'
 kill_app_template = '%(server)s:%(port)d/ws/v1/cluster/apps/%(app_id)s/state'
 
 
 def kill_zombie_jobs(task_id):
-    logger.info('checking if jobs with Airflow unique identifier %s is running...' % args.task_id)
+    logger.info('checking if jobs with Airflow unique identifier %s is running...' % task_id)
 
     job_url = zombie_apps_template % {'server': job_rm, 'port': job_rm_port, 'user': user, 'states': valid_states,
-                                      'task_id': args.task_id}
+                                      'task_id': task_id}
     resp = json.load(urllib.urlopen(job_url))
     apps = resp['apps']
 
     if apps is not None:
         try:
-            id = apps['app'][0]['id']
-            logger.info('found and killing %s...' % id)
+            for app in apps['app']:
+                id = app['id']
+                logger.info('found and killing %s...' % id)
 
-            # For transition period, fail the job
-            raise Exception('zombie job found, failing...')
+                # For transition period, fail the job
+                raise Exception('zombie job found, failing...')
 
-            app_kill_url = kill_app_template % {'server': job_rm, 'port': job_rm_port, 'app_id': id}
-            r = requests.put(app_kill_url, data=json.dumps({'state': 'KILLED'}))
-            r.raise_for_status()
+                app_kill_url = kill_app_template % {'server': job_rm, 'port': job_rm_port, 'app_id': id}
+                r = requests.put(app_kill_url, data=json.dumps({'state': 'KILLED'}))
+                r.raise_for_status()
 
         except Exception as e:
             logger.error('could not kill zombie job')
