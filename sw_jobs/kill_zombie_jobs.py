c0d73ca118ba980cb38727542f2e3cc864a740ce
amitr <amitr@similargroup.com>
2016-05-05 12:14:56 +0300

    SIM-9930: Kill Zombie Jobs Attempt to actually kill job

--- sw_jobs/kill_zombie_jobs.py
+++ sw_jobs/kill_zombie_jobs.py
@@ -39,7 +39,7 @@ class ZombieKiller:
                     logger.info('found and killing %s...' % id)
 
                     # For transition period, fail the job
-                    raise ZombieJobFoundException(task_id)
+                    #raise ZombieJobFoundException(task_id)
 
                     app_kill_url = kill_app_template % {'server': job_rm, 'port': job_rm_port, 'app_id': id}
                     r = requests.put(app_kill_url, data=json.dumps({'state': 'KILLED'}))
