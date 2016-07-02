078d06db5ed60778d0f07795de501d2abb421776
Felix Vaisman <felixv@similargroup.com>
2016-07-02 20:01:11 +0300

    adapt taks io buffer in hive jobs

--- hive/hive_runner.py
+++ hive/hive_runner.py
@@ -255,7 +255,8 @@ def run_hive_job(hql, job_name, num_of_reducers, log_dir, slow_start_ratio=None,
            "-hiveconf", "hive.stats.fetch.column.stats=true",
            "-hiveconf", "mapreduce.child.java.opts=-Xmx%(memory)dm -Xms%(memory)dm" % {'memory': task_memory},
            "-hiveconf", "mapreduce.reduce.memory.mb=%d" % int(task_memory * (1 + TASK_MEMORY_OVERHEAD)),
-           "-hiveconf", "mapreduce.map.memory.mb=%d" % int(task_memory * (1 + TASK_MEMORY_OVERHEAD))
+           "-hiveconf", "mapreduce.map.memory.mb=%d" % int(task_memory * (1 + TASK_MEMORY_OVERHEAD)),
+           "-hiveconf", "mapreduce.task.io.sort.mb=%d" % min(task_memory / 10, 256)
            ]
 
     if codec:
