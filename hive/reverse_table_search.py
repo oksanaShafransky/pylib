e76c9c7999fa56974ad8fb2eab418e796f8ae133
Barak Gitsis <barakg@similarweb.com>
2016-07-14 17:46:16 +0300

    add psycopg2==2.6.2 to requirements.txt

--- hive/reverse_table_search.py
+++ hive/reverse_table_search.py
@@ -1,4 +1,5 @@
 import psycopg2
+import sys
 
 __author__ = 'barakg'
 
@@ -9,19 +10,19 @@ Example query:
 """
 
 
-def find_table_name(location):
+def find_table_name(location, print_query=True):
     with psycopg2.connect("postgresql://readonly:readonly@hive-postgres-mrp.service.production:5432/hive") as conn:
         with conn.cursor() as cur:
             location = '%' + location + '%'
             qry = """select db_name, table_name from hive_table_location where location_uri like %s;"""
-            print cur.mogrify(qry, [location])
+            if print_query:
+                sys.stdout.write(cur.mogrify(qry, [location]) + '\n')
             cur.execute(qry, [location])
             return cur.fetchall()
 
 
-def get_table_names(location):
-    print '\n'.join(map(lambda db_table: '%s.%s' % db_table, find_table_name(location)))
+def get_table_names(location, print_query):
+    print '\n'.join(map(lambda db_table: '%s.%s' % db_table, find_table_name(location, print_query)))
 
 
-# column names:  db_name, table_name ,location_uri
-get_table_names(location='daily/aggregate/aggkey=SiteCountrySourceKey')
+get_table_names(location='daily/aggregate/aggkey=SiteCountrySourceKey', print_query=False)
