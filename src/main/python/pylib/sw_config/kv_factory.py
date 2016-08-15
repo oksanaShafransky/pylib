9764eaecd7cbac3a6cdda4104e9efaac5e174145
Felix Vaisman <felixv@similargroup.com>
2016-08-15 18:54:10 +0300

    fix kv loading

--- src/main/python/pylib/sw_config/kv_factory.py
+++ src/main/python/pylib/sw_config/kv_factory.py
@@ -1,21 +1,13 @@
 __author__ = 'Felix'
 
+from pydoc import locate
 import json
 
 from composite_kv import CompositeConfigurationProxy
 
 
-def _load_class(cls):
-    name_parts = cls.split('.')
-    curr_mod = __import__(name_parts[0])
-    for sub_mod in name_parts[1:]:
-        curr_mod = getattr(curr_mod, sub_mod)
-
-    return curr_mod
-
-
 def create_proxy(proxy_cls, params):
-    return _load_class(proxy_cls)(**params)
+    return locate(proxy_cls)(**params)
 
 
 def provider_from_config(config_str):
