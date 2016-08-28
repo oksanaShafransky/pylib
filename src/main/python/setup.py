a4c1cf4960436140c76799a2683fea656d6ed12b
jeniag <jeniag@similargroup.com>
2016-08-28 16:45:24 +0300

    Update test packages

--- src/main/python/setup.py
+++ src/main/python/setup.py
@@ -6,6 +6,6 @@ setup(
     zip_safe=True,
     requires=['invoke', 'tld', 'mrjob', 'redis'],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'snakebite', 'invoke==0.12.2', 'redis'],
+    tests_require=['pytest', 'boto==2.42.0', 'snakebite', 'invoke==0.12.2', 'redis', 'ujson','mrjob==0.5.2'],
     test_suite='tests'
 )
