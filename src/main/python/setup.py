9f27832a9991da85ab227b8b9f849e410dfd0269
jeniag <jeniag@similargroup.com>
2016-08-29 19:19:44 +0300

    Add a bunch of requirements to packages

--- src/main/python/setup.py
+++ src/main/python/setup.py
@@ -4,8 +4,8 @@ setup(
     name="sw_pylib",
     packages=find_packages(),
     zip_safe=True,
-    requires=['invoke', 'tld', 'mrjob', 'redis'],
+    install_requires=['invoke', 'tld', 'mrjob', 'redis', 'snakebite', 'invoke==0.12.2', 'redis', 'ujson==1.33','mrjob==0.5.2'],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'boto==2.42.0', 'snakebite', 'invoke==0.12.2', 'redis', 'ujson','mrjob==0.5.2'],
+    tests_require=['pytest', 'boto==2.42.0', 'snakebite', 'invoke==0.12.2', 'redis', 'ujson==1.33','mrjob==0.5.2'],
     test_suite='tests'
 )
