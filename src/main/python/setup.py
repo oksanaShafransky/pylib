c078bec2ed0fc4efb75dbc965a58d52435a4b2c3
iddoa <iddo.aviram@similarweb.com>
2016-10-20 23:17:57 +0300

    Adding a missing pylib test dependency

--- src/main/python/setup.py
+++ src/main/python/setup.py
@@ -6,6 +6,6 @@ setup(
     zip_safe=True,
     install_requires=['invoke==0.12.2', 'tld', 'mrjob', 'redis', 'snakebite', 'redis', 'ujson==1.33', 'mrjob==0.5.2', 'consulate==0.6.0', 'python-etcd==0.3.3', 'urllib3==1.16'],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'boto==2.42.0', 'snakebite', 'invoke==0.12.2', 'redis', 'ujson==1.33', 'mrjob==0.5.2'],
+    tests_require=['pytest', 'python-dateutil', 'boto==2.42.0', 'snakebite', 'invoke==0.12.2', 'redis', 'ujson==1.33', 'mrjob==0.5.2'],
     test_suite='tests'
 )
