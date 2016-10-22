2603d8f832b8b727d83ce08fc71516159ad38cf2
iddoa <iddo.aviram@similarweb.com>
2016-10-22 10:27:35 +0300

    Executing ptask directly from the package via an entrypoint

--- src/main/python/setup.py
+++ src/main/python/setup.py
@@ -7,5 +7,8 @@ setup(
     install_requires=['invoke==0.12.2', 'tld', 'mrjob', 'redis', 'snakebite', 'redis', 'ujson==1.33', 'mrjob==0.5.2', 'consulate==0.6.0', 'python-etcd==0.3.3', 'urllib3==1.16'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'python-dateutil', 'boto==2.42.0', 'snakebite', 'invoke==0.12.2', 'redis', 'ujson==1.33', 'mrjob==0.5.2'],
-    test_suite='tests'
+    test_suite='tests',
+    entry_points={
+        'console_scripts' :['ptask = pylib.tasks.ptask_invoke:main']
+    }
 )
