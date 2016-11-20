fca749a130d51abbb90f7b08af0e1980dcd4666b
Oded Fried <oded.fried@similarweb.com>
2016-11-20 10:05:11 +0200

    generic hive query for loading to hbase

--- pylib/hive/queries.py
+++ pylib/hive/queries.py
@@ -1,7 +1,7 @@
 __author__ = 'Felix'
 
 from pylib.hive.common import *
-from pylib.hive.table_utils import TableProvided
+from pylib.hive.table_utils import TableProvided, HBaseTableProvided
 
 
 @formatted
@@ -30,3 +30,31 @@ def sum_query(dt, mode, mode_type, daily_table, daily_table_path, sum_table, sum
                                             (filters or [])
                                         )
                                     }
+
+
+@formatted
+@deploy_jars
+@TableProvided(alias='source_table', table_name_resolver=lambda **kwargs: '%s.%s' % (kwargs['hive_db'],kwargs['source_table_name']), path_param='source_table_path')
+@HBaseTableProvided(alias='target_hbase_table', table_name_resolver=lambda **kwargs: '%s.%s' % (kwargs['hive_db'], kwargs['target_hive_template']), hbase_table_name_param='hbase_table_name')
+def load_to_hbase_query(date, mode, mode_type, hive_db, source_table_name, source_table_path, target_hive_template, hbase_table_name, deploy_path, cols, where_filters=None, temp_functions=None, **kwargs):
+    year, month, day = parse_date(date)
+    return '''
+        use %(hive_db)s;
+        %(create_functions)s
+        INSERT OVERWRITE TABLE %(target_hbase_table)s
+        SELECT %(cols)s
+        FROM %(source_table)s
+        where %(where_clause)s;
+        ''' \
+           % \
+           {
+               'hive_db': hive_db,
+               'create_functions': '\n'.join(["CREATE TEMPORARY FUNCTION %s AS '%s';" % (name, as_class)
+                                              for (name, as_class) in temp_functions]),
+               'cols': ','.join(cols),
+               'target_hbase_table': kwargs['target_hbase_table'],
+               'source_table': kwargs['source_table'],
+               'where_clause': ' AND '.join(
+                   [get_range_where_clause(year, month, day, mode, mode_type)] +
+                   (where_filters or []))
+           }
