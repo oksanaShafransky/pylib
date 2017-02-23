8be0bbd04afd7e0249e00a6fa447120677cfc212
Oded Fried <oded.fried@similarweb.com>
2017-02-23 13:47:51 +0200

    hbase validation tool:  fix retries + fix error messages

--- pylib/hbase/hbase_utils.py
+++ pylib/hbase/hbase_utils.py
@@ -19,22 +19,21 @@ def validate_records_per_region(table_name, columns = None, minimum_regions_coun
 
     #skipping last region, it can have few keys
     for region in regions[:-1]:
+        timeout_retry = 0
         start_key = region['start_key']
         sc = tbl.scan(row_start=start_key, columns=columns, limit=rows_per_region)
-        for i in range(rows_per_region):
-            timeout_retry = 0
-
+        for row_num in range(rows_per_region):
             try:
-                next = sc.next()
+                sc.next()
             except StopIteration:
-                print "to few keys in region: %s\n starting key: %s\n (%d < %d)" % (region['name'], start_key, i, rows_per_region)
+                print "to few keys in region: %s\n starting key: %s\n (%d < %d)" % (region['name'], start_key, row_num, rows_per_region)
                 return False
             except socket_error:
                 timeout_retry += 1
                 if timeout_retry < TIMEOUT_RETRIES:
-                    print 'socket timeout in region number %d. reloading table (retry %d)' % (i, timeout_retry)
+                    print 'socket timeout in region %s, row:%d. reloading table (retry %d)' % (region['name'], row_num, timeout_retry)
                     tbl = get_hbase_table(table_name, cluster_name)
-                    sc = tbl.scan(row_start=start_key, columns=columns, limit=rows_per_region - i)
+                    sc = tbl.scan(row_start=start_key, columns=columns, limit=rows_per_region - row_num)
                 else:
                     print 'could not read table'
                     return False
