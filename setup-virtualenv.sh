6834e5d5a888eb8b5c6c9ee085a817eec1e317f7
drorgivoli <dror.givoli@similarweb.com>
2017-05-15 10:50:17 +0300

    moved setuptools tp reqs

--- setup-virtualenv.sh
+++ setup-virtualenv.sh
@@ -4,4 +4,4 @@ virtualenv --no-setuptools .
 source bin/activate
 # wget https://bootstrap.pypa.io/ez_setup.py -O - | python
 # bin/easy_install pip
-bin/pip install setuptools==35.0.2
+# bin/pip install setuptools==35.0.2
