11d27a0b9633b12212069d95fabed51286703f09
drorgivoli <dror.givoli@similarweb.com>
2017-05-15 14:50:53 +0300

    use system-site-packages

--- setup-virtualenv.sh
+++ setup-virtualenv.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-virtualenv .
+virtualenv --system-site-packages .
 source bin/activate
 # wget https://bootstrap.pypa.io/ez_setup.py -O - | python
 bin/easy_install pip
 bin/pip install pip==9.0.1
-# bin/pip install setuptools==35.0.2
+bin/pip install setuptools==35.0.2
 
 
 # virtualenv --no-setuptools .
