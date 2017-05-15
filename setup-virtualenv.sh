bbffec9c4504ceada726776a574834ba7af424c0
drorgivoli <dror.givoli@similarweb.com>
2017-05-15 11:18:39 +0300

    added pip

--- setup-virtualenv.sh
+++ setup-virtualenv.sh
@@ -3,5 +3,5 @@
 virtualenv --no-setuptools .
 source bin/activate
 # wget https://bootstrap.pypa.io/ez_setup.py -O - | python
-# bin/easy_install pip
+bin/easy_install pip
 # bin/pip install setuptools==35.0.2
