7c3d0b475eccd7ba275787179f9f94a5736b7ace
drorgivoli <dror.givoli@similarweb.com>
2017-05-15 11:57:35 +0300

    upgrade pip

--- setup-virtualenv.sh
+++ setup-virtualenv.sh
@@ -4,6 +4,7 @@ virtualenv .
 source bin/activate
 # wget https://bootstrap.pypa.io/ez_setup.py -O - | python
 bin/easy_install pip
+bin/pip install pip==9.0.1
 # bin/pip install setuptools==35.0.2
 bin/pip install six==1.10.0
 
