5c46b0a2a83d009616fb037e9f61c1e63c927a12
drorgivoli <dror.givoli@similarweb.com>
2017-05-15 17:37:52 +0300

    put wheel in setup_requires

--- setup.py
+++ setup.py
@@ -5,9 +5,23 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     zip_safe=True,
-    install_requires=['invoke==0.12.2', 'tld', 'redis', 'happybase', 'snakebite', 'thriftpy==0.3.8', 'ujson==1.33',
-                      'mrjob==0.5.2', 'consulate==0.6.0', 'python-etcd==0.3.3', 'urllib3==1.16'],
-    setup_requires=['pytest-runner'],
+    install_requires=[
+        'invoke==0.12.2',
+        'tld',
+        'redis',
+        'happybase',
+        'snakebite',
+        'thriftpy==0.3.8',
+        'ujson==1.33',
+        'mrjob==0.5.2',
+        'consulate==0.6.0',
+        'python-etcd==0.3.3',
+        'urllib3==1.16'
+    ],
+    setup_requires=[
+        'pytest-runner'
+        'wheel==0.29.0'
+    ],
     tests_require=[
         'pytest',
         'python-dateutil',
@@ -16,7 +30,8 @@ setup(
         'invoke==0.12.2',
         'redis',
         'ujson==1.33',
-        'mrjob==0.5.2'],
+        'mrjob==0.5.2'
+    ],
     test_suite='tests',
     entry_points={
         'console_scripts': ['ptask = pylib.tasks.ptask_invoke:main']
