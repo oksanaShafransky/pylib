f22c39425d4096eee736f9560d9f6b7c02e9578f
drorgivoli <dror.givoli@similarweb.com>
2017-05-15 11:25:26 +0300

    leave setuptools

--- setup-virtualenv.sh
+++ setup-virtualenv.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-virtualenv --no-setuptools .
+virtualenv .
 source bin/activate
 # wget https://bootstrap.pypa.io/ez_setup.py -O - | python
 bin/easy_install pip
 # bin/pip install setuptools==35.0.2
+
+
+# virtualenv --no-setuptools .
+# source bin/activate
+# wget https://bootstrap.pypa.io/ez_setup.py -O - | python
+# bin/easy_install pip
