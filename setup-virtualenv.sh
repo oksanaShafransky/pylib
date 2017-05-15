04a18d8ea12dc9c78adac705a40127f912d3b585
drorgivoli <dror.givoli@similarweb.com>
2017-05-15 11:45:58 +0300

    install six in bash

--- setup-virtualenv.sh
+++ setup-virtualenv.sh
@@ -5,6 +5,7 @@ source bin/activate
 # wget https://bootstrap.pypa.io/ez_setup.py -O - | python
 bin/easy_install pip
 # bin/pip install setuptools==35.0.2
+bin/pip install six==1.10.0
 
 
 # virtualenv --no-setuptools .
