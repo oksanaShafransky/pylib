cc33c73e2fd02998f4ce4d5c50e5f2fbf71f0375
drorgivoli <dror.givoli@similarweb.com>
2017-05-16 09:31:14 +0300

    cleanup bash

--- setup-virtualenv.sh
+++ setup-virtualenv.sh
@@ -2,14 +2,5 @@
 
 virtualenv --no-site-packages .
 source bin/activate
-# wget https://bootstrap.pypa.io/ez_setup.py -O - | python
 bin/easy_install pip
 bin/pip install pip==9.0.1
-#bin/pip install setuptools==35.0.2
-#bin/pip install boto==2.35.0
-
-
-# virtualenv --no-setuptools .
-# source bin/activate
-# wget https://bootstrap.pypa.io/ez_setup.py -O - | python
-# bin/easy_install pip
