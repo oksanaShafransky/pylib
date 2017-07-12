57979321f856bb54881164337e872b3908c426cb
drorgivoli <dror.givoli@similarweb.com>
2017-07-12 13:37:18 +0300

    added boto version

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     install_requires=[
         'ujson==1.33',
         'thriftpy==0.3.8',
+        'boto==2.38.0',
         'snakebite==2.11.0',
         'happybase==0.9',
         'requests==2.14.2',
