6e51b97b25d1352382372e276d7521d7e73bd344
lior.bercovits <lior.bercovits@similarweb.com>
2017-08-27 13:42:49 +0300

    date_utils: Separated get_dates_list inner function from actual range logic

--- pylib/common/date_utils.py
+++ pylib/common/date_utils.py
@@ -2,19 +2,36 @@ from datetime import date
 from dateutil.relativedelta import relativedelta
 
 
-def get_dates_range(end, num_leaps, leap_interval='days', date_offset=0):
+def get_dates_range(end, range_len, step_type='days', step_size=1, date_offset=0):
     """
-    returns list of datetime objects for each date in range starting from (end - offset) and going backwards.
+    Returns list of datetime objects for each date in range starting from (end - offset) and going backwards.
+    Step interval is step_type*step_size
 
-    :param end: this param is aimed for dag execution_date. for use cases where we need dates from current date and back
-    :param leap_interval: jumps interval size ('years', 'months', 'weeks', 'days')
-    :param num_leaps: number of dates in range
+    :param range_len:   list length output dates range
+    :param step_type:   days/weeks/months/years (relativedelta API)
+    :param step_size:   sets the actual step size, depends on step_type
+    :param end:         this param is aimed for dag execution_date. for use cases where we need dates from current date and back
     :param date_offset: date offset from end date (date_offset == 1 -> end date is excluded from range)
     """
     truncated_end = date(end.year, end.month, end.day)
+    step = relativedelta(**{step_type:step_size})
+    range_end = truncated_end - step*date_offset
+    range_start = range_end - step*(range_len-1)
+    return list(reversed(get_dates_list(range_start, range_end, step)))
+
+
+def get_dates_list(start, end, step=relativedelta(days=1)):
+    """
+    returns list of datetime objects for each date in range starting from (end - offset) and going backwards.
+
+    :param start: Start date for the range
+    :param end:  End date for the range
+    :param step: Step size for the range
+    """
     dates_range = []
-    delta = relativedelta()
-    for i in range(date_offset, num_leaps + date_offset):
-        setattr(delta, leap_interval, i)
-        dates_range.append(truncated_end - delta)
+    curr = start
+    while curr <= end:
+        dates_range.append(curr)
+        curr += step
     return dates_range
+
--- tests/common/test_date_utils.py
+++ tests/common/test_date_utils.py
@@ -26,6 +26,11 @@ class TestDateUtils(TestCase):
         assert output == expected
 
     def test_get_months_range_offset(self):
-        output = date_utils.get_dates_range(date(year=2017, month=6, day=1), 3, 'months', 2)
+        output = date_utils.get_dates_range(date(year=2017, month=6, day=1), 3, 'months', date_offset=2)
         expected = [date(year=2017, month=4, day=1), date(year=2017, month=3, day=1), date(year=2017, month=2, day=1)]
+        assert output == expected
+
+    def test_get_weeks_range_step_size_2(self):
+        output = date_utils.get_dates_range(date(year=2016, month=2, day=2), 3, 'weeks', step_size=2)
+        expected = [date(year=2016, month=2, day=2), date(year=2016, month=1, day=19), date(year=2016, month=1, day=5)]
         assert output == expected
