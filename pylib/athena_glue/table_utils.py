e36dcd9deb1e2b4caad16e47c17e1d7b0ea760d5
iddoaviram <iddo.aviram@similarweb.com>
2017-12-29 16:53:33 +0200

    Misc name and code style fixes

--- pylib/athena_glue/table_utils.py
+++ pylib/athena_glue/table_utils.py
@@ -16,12 +16,12 @@ def __filter_out_dict(dictionary, keys):
     return {k: v for k, v in dictionary.items() if k not in keys}
 
 
-def repair_checkpointed_table(db, table_name):
+def repair_table(db, table_name):
     athena_client = get_athena_client()
-    response = athena_client.start_query_execution(
+    repair_query_response = athena_client.start_query_execution(
         QueryString='msck repair table {}'.format(table_name),
         QueryExecutionContext={
-            'Database': '{}'.format(db)
+            'Database': db
         },
         ResultConfiguration={
             'OutputLocation': 's3://sw-dag-published-v2/tmp/',
@@ -30,58 +30,57 @@ def repair_checkpointed_table(db, table_name):
             }
         }
     )
-    query_execution_id = response['QueryExecutionId']
+    repair_query_execution_id = repair_query_response['QueryExecutionId']
 
-    def query_finished(response):
-        return response['QueryExecution']['Status']['State'] in ['SUCCEEDED','FAILED','CANCELLED']
+    def is_query_finished(query_response):
+        return query_response['QueryExecution']['Status']['State'] in ['SUCCEEDED', 'FAILED', 'CANCELLED']
 
     terminal_response = polling.poll(
-        lambda: athena_client.get_query_execution(QueryExecutionId=query_execution_id),
-        check_success=query_finished, step=0.5, timeout=7)
+        lambda: athena_client.get_query_execution(
+            QueryExecutionId=repair_query_execution_id),
+        check_success=is_query_finished, step=0.5, timeout=7)
     assert terminal_response['QueryExecution']['Status']['State'] == 'SUCCEEDED'
     return True
 
 
-def create_new_checkpoint(db, table_name, new_checkpoint_id):
+def create_checkpoint(db, table_name, new_checkpoint_id):
     client = get_glue_client()
-    existing_table_def_response = client.get_table(
-        DatabaseName='{}'.format(db),
-        #Name='{}_latest'.format(checkpointed_table_name)
+    table_def_response = client.get_table(
+        DatabaseName=db,
         Name=table_name
     )
-    existing_table_def = existing_table_def_response['Table']
-    new_table_def = __filter_out_dict(existing_table_def, ['UpdateTime', 'CreatedBy', 'CreateTime'])
-    new_table_name = existing_table_def['Name'] + '_' + new_checkpoint_id
-    new_table_def['Name'] = new_table_name
+    table_def = table_def_response['Table']
+    checkpoint_table_def = __filter_out_dict(table_def, ['UpdateTime', 'CreatedBy', 'CreateTime'])
+    checkpoint_table_name = '{}_{}'.format(table_def['Name'], new_checkpoint_id)
+    checkpoint_table_def['Name'] = checkpoint_table_name
     client.create_table(DatabaseName='{}'.format(db),
-                        TableInput=new_table_def)
+                        TableInput=checkpoint_table_def)
 
-    prev_partitions_response = client.get_partitions(
-        DatabaseName='{}'.format(db),
-        #TableName='{}_latest'.format(checkpointed_table_name)
+    table_partitions_response = client.get_partitions(
+        DatabaseName=db,
         TableName=table_name
     )
 
-    new_partitions_response=client.batch_create_partition(
-        DatabaseName='{}'.format(db),
-        TableName='{}'.format(new_table_name),
-        PartitionInputList=map(lambda d:__filter_out_dict(d,['CreationTime','TableName','DatabaseName']),
-                               prev_partitions_response['Partitions'])
+    checkpoint_table_partitions_response = client.batch_create_partition(
+        DatabaseName=db,
+        TableName=checkpoint_table_name,
+        PartitionInputList=map(lambda d: __filter_out_dict(d, ['CreationTime', 'TableName', 'DatabaseName']),
+                               table_partitions_response['Partitions'])
     )
+    assert not checkpoint_table_partitions_response['Errors']
 
-    assert not new_partitions_response['Errors']
-
-    new_table_def['Name'] = existing_table_def['Name']
-    if 'from_' in existing_table_def['StorageDescriptor']['Location']:
+    table_new_def = checkpoint_table_def
+    table_new_def['Name'] = table_def['Name']
+    if 'from_' in table_def['StorageDescriptor']['Location']:
         new_table_location = '{}from_{}/' \
-            .format(existing_table_def['StorageDescriptor']['Location'].rsplit('from_',1)[0], new_checkpoint_id)
+            .format(table_def['StorageDescriptor']['Location'].rsplit('from_', 1)[0], new_checkpoint_id)
     else:
         new_table_location = '{}from_{}/' \
-            .format(existing_table_def['StorageDescriptor']['Location'], new_checkpoint_id)
-    new_table_def['StorageDescriptor']['Location'] = new_table_location
+            .format(table_def['StorageDescriptor']['Location'], new_checkpoint_id)
+    table_new_def['StorageDescriptor']['Location'] = new_table_location
     response = client.update_table(
         DatabaseName='{}'.format(db),
-        TableInput=new_table_def
+        TableInput=table_new_def
     )
-
-    return response
+    assert response['ResponseMetadata']['HTTPStatusCode'] == 200
+    return True
--- tests/athenaglue/test_table_utils.py
+++ tests/athenaglue/test_table_utils.py
@@ -1,14 +1,16 @@
-import pytest
+from __future__ import print_function
 import pprint
-from pylib.athena_glue.table_utils import repair_checkpointed_table,create_new_checkpoint
+from pylib.athena_glue.table_utils import repair_table, create_checkpoint
 
 
 class TestTableUtils:
+    def __init__(self):
+        pass
+
     def test_repair_table(self):
-        result = repair_checkpointed_table('iddoa','iddo_downloads')
-        print result
+        result = repair_table('iddoa', 'iddo_downloads')
+        print(result)
 
     def test_create_new_version(self):
-        result = create_new_checkpoint('iddoa','iddo_downloads','2')
+        result = create_checkpoint('iddoa', 'iddo_downloads', '2')
         pprint.pprint(result)
-
