c8de817aeea6a519ec43e3fee46d851f8f0d2836
Barak Gitsis <barakg@similarweb.com>
2018-05-28 21:55:12 +0300

    add get_namenode_url_from_hdfs_site

--- pylib/docker/docker_utils.py
+++ pylib/docker/docker_utils.py
@@ -1,5 +1,6 @@
 import docker
 import os
+import xml.etree.ElementTree as ET
 
 DOCKER_HOST_PORT = '2375'
 
@@ -12,7 +13,7 @@ def get_my_container_id():
     return os.environ.get('HOSTNAME')
 
 
-def docker_image_full_name():
+def get_namenode_url_from_hdfs_site():
     docker_gate = get_my_docker_gate()
     if docker_gate is None:
         return None
@@ -21,3 +22,9 @@ def docker_image_full_name():
 
     return cli.inspect_container(get_my_container_id())['Config']['Image']
 
+
+def get_namenode_url_from_hdfs_site(hdfs_site_path='/etc/hadoop/conf/hdfs-site.xml'):
+    for prop in ET.parse(hdfs_site_path).findall("property"):
+        if 'dfs.namenode.rpc-address' in prop.find('name').text:
+            namenode_url = prop.find('value').text
+            return namenode_url
