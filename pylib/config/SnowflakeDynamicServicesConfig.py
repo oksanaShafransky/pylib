a3070baabdb49ba75abd19e2908e0700f5ac8e55
raphael.peretz <raphael.peretz@similarweb.com>
2018-08-15 15:39:35 +0300

    Rename and read default env

--- pylib/config/SnowflakeDynamicServicesConfig.py
+++ pylib/config/SnowflakeDynamicServicesConfig.py
@@ -1,12 +1,23 @@
 import requests
+import os
 
 
-class DynamicServicesConfig:
-    def __init__(self, url='http://bigdata-snowflake-ds-aws-production.op-us-east-1.bigdata-grid.int.similarweb.io', path_in_url="/serviceResolution"):
+class SnowflakeDynamicServicesConfig:
+    def_env = None
+
+    def __init__(self, url='http://bigdata-snowflake-ds-aws-production.op-us-east-1.bigdata-grid.int.similarweb.io',
+                 path_in_url="/serviceResolution"):
         self.base_url = url
         self.path_in_url = path_in_url
+        try:
+            self.def_env = os.environ['SNOWFLAKE_ENV']
+        except Exception:
+            print("ERROR: failed to read snowflake environment variable")
+            raise
 
     def get_service_name(self, env=None, service_name=None):
+        if env is None:
+            env = self.def_env
         # Must be first line in the function
         service_args = locals().items()
         # Must be first line in the function
@@ -21,4 +32,4 @@ class DynamicServicesConfig:
         return str(r.text)
 
 
-services_config = DynamicServicesConfig()
+snowflake_services_config = SnowflakeDynamicServicesConfig()
