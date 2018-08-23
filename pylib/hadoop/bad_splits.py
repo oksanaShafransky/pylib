13b3bd747df0355fec148109fc0d4cbe5f9f0668
Felix Vaisman <felixv@similarweb.com>
2018-08-23 15:31:56 +0300

    allow corrupt cleanup to work on both clusters

--- pylib/hadoop/bad_splits.py
+++ pylib/hadoop/bad_splits.py
@@ -5,8 +5,11 @@ import json
 import urllib
 import logging
 import re
+import socket
 
-job_history_server = 'http://hive-server2-mrp.service.production'
+ip_addr = socket.gethostbyname(socket.gethostname())
+in_aws = ip_addr.startswith('10.10')
+job_history_server = 'http://hive-server2-mrp.service.production' if in_aws else 'http://hdfs-namenode-mrp.service.production'
 job_history_port = 19888
 
 job_endpoint = '%(server)s:%(port)d/ws/v1/history/mapreduce/jobs/%(job_id)s'
@@ -27,12 +30,12 @@ def get_relative_file(split):
 LOG_PORT = 4545
 log_url_re = re.compile(r"(.*/jobhistory/logs.*:)(\d+)(.*)")
 def fix_log_url(unchecked_log_url):
-    return unchecked_log_url
-    """
-    log_match = re.split(log_url_re, unchecked_log_url)
-    log_match[2] = str(LOG_PORT)   # replace with correct port
-    return ''.join(log_match)
-    """
+    if not in_aws:
+        log_match = re.split(log_url_re, unchecked_log_url)
+        log_match[2] = str(LOG_PORT)   # replace with correct port
+        return ''.join(log_match)
+    else:
+        return unchecked_log_url
 
 
 def get_bad_splits(log_str):
