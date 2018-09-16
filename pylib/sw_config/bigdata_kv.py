8ff50b1137dfc14e5a29dd01d0ff3e2e42309560
steven <steven@similarweb.com>
2018-09-16 16:35:34 +0300

    Use snowflake

--- pylib/sw_config/bigdata_kv.py
+++ pylib/sw_config/bigdata_kv.py
@@ -1,29 +1,21 @@
+from pylib.config.SnowflakeConfig import SnowflakeConfig
 from pylib.sw_config.kv_factory import provider_from_config
 from pylib.sw_config.composite_kv import PrefixedConfigurationProxy
 
 
 class KeyValueConfig(object):
+    consul_endpoint = SnowflakeConfig().get_service_name(service_name='consul')
     _kv_prod_conf = """
               [
                 {
                      "class": "pylib.sw_config.consul.ConsulProxy",
-                     "server":"consul.service.production"
+                     "server":"{}"
                 }
               ]
-    """
-
-    _kv_stage_conf = """
-                  [
-                    {
-                         "class": "pylib.sw_config.consul.ConsulProxy",
-                         "server":"consul.service.staging"
-                    }
-                  ]
-        """
+    """.format(consul_endpoint)
 
     base_kv = {
-        'production': provider_from_config(_kv_prod_conf),
-        'staging': provider_from_config(_kv_stage_conf)
+        'production': provider_from_config(_kv_prod_conf)
     }
 
 
