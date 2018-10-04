62df3dff152f3e9ed6d41eba6524083fc066c158
raphael.peretz <raphael.peretz@similarweb.com>
2018-10-04 13:51:35 +0300

    Retrive consul from snowflake

--- pylib/sw_config/bigdata_kv.py
+++ pylib/sw_config/bigdata_kv.py
@@ -6,7 +6,7 @@ from pylib.config.SnowflakeConfig import SnowflakeConfig
 class KeyValueConfig(object):
     def __init__(self, snowflake_env):
         self._kv_prod_conf = [{'class': "pylib.sw_config.consul.ConsulProxy",
-                               'server': "consul.service.production"}]
+                               'server': SnowflakeConfig(snowflake_env).get_service_name(service_name="consul")}]
         self._kv_stage_conf = """
                       [
                         {
@@ -23,5 +23,5 @@ class KeyValueConfig(object):
 
 
 def get_kv(env='production', purpose='bigdata', snowflake_env=None):
-    basic_kv = KeyValueConfig(env).get_base_kv()[env.lower()]
+    basic_kv = KeyValueConfig(snowflake_env).get_base_kv()[env.lower()]
     return basic_kv if purpose is None else PrefixedConfigurationProxy(basic_kv, prefixes=[purpose])
