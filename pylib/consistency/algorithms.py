c1b8ef1b086af53a88a05c269675eb017e32b086
drorgivoli <dror.givoli@similarweb.com>
2018-11-20 13:54:31 +0200

    added change_percentage test

--- pylib/consistency/algorithms.py
+++ pylib/consistency/algorithms.py
@@ -1,8 +1,14 @@
 import numpy as np
 
 
+def change_percentage(samples):
+    current_sample = samples[-1:][0]
+    history_samples = samples[:-1]
+    history_average = np.mean(history_samples)
+    return ((current_sample - history_average) / history_average) * 100
 
-def distince_from_avarage(samples):
+
+def distance_from_average(samples):
     current_sample = samples[-1:][0]
     history_samples = samples[:-1]
     return np.abs(
@@ -24,30 +30,13 @@ def calc_regression(samples, min_threshold=1.8):
     dx = np.sqrt(x1 * np.transpose(x1))
     res = np.abs(beta1) / (SRSS / dx) - min_threshold
 
-    # print(res)
-    # print(beta1)
     if res < 0:
         res = 0
     if res > 1.0:
         res = 1.0
-    if res > 0:
-        slope_assumed = 1.0
-    else:
-        slope_assumed = 0.0
-
-    # print(np.sign(beta1))
-    # print(np.sign(beta1) * res)
-    # print(float(np.sign(beta1) * res))
-    return float(np.sign(beta1) * res), float(slope_assumed * beta1[0, 0])
-
-
-samples = [
-    0.008419494,
-    0.008474652,
-    0.008574391,
-    0.00863166,
-    0.008802582,
-    0.008430844,
-    0.008347507
-]
-
+    # if res > 0:
+    #     slope_assumed = 1.0
+    # else:
+    #     slope_assumed = 0.0
+    # return float(np.sign(beta1) * res), float(slope_assumed * beta1[0, 0])
+    return res
