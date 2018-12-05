ccb0a52adfcd35134a133bcedbbac2398f238c8b
drorgivoli <dror.givoli@similarweb.com>
2018-12-05 09:41:31 +0200

    added parameter to discard prefix

--- pylib/sw_config/bigdata_kv.py
+++ pylib/sw_config/bigdata_kv.py
@@ -4,7 +4,7 @@ from pylib.sw_config.consul import ConsulProxy
 from pylib.sw_config.composite_kv import PrefixedConfigurationProxy
 
 
-def get_kv(purpose='bigdata', snowflake_env=None):
+def get_kv(purpose='bigdata', snowflake_env=None, append_prefix=True):
     # import snowflake config in function to allow mock in unit tests
     from pylib.config.SnowflakeConfig import SnowflakeConfig
     consul_snowflake_key = purpose + '-consul-kv'
@@ -16,7 +16,10 @@ def get_kv(purpose='bigdata', snowflake_env=None):
         token=consul_properties.get('token')  # user .get to allow None token
     )
 
-    return basic_kv if 'prefix' not in consul_properties else PrefixedConfigurationProxy(
+    if not append_prefix or 'prefix' not in consul_properties:
+        return basic_kv
+
+    return PrefixedConfigurationProxy(
         underlying_proxy=basic_kv,
         prefixes=[consul_properties['prefix']],
         optional_get_prefixes=consul_properties.get('optional_prefix')
