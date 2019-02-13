fc86506a2c4cf9ed14f741095f70bd16ca284eda
raphael.peretz <raphael.peretz@similarweb.com>
2019-02-13 19:36:10 +0200

    revert multiple buckets

--- pylib/tasks/data.py
+++ pylib/tasks/data.py
@@ -1,14 +1,13 @@
 import logging
-import json
 
 from pylib.common.date_utils import generate_date_suffix
 from pylib.hadoop.hdfs_util import get_size as size_on_hdfs, file_exists as exists_hdfs, directory_exists as dir_exists_hdfs
 from pylib.aws.s3.inventory import get_size as size_on_s3, does_exist as exists_s3
-from pylib.config.SnowflakeConfig import SnowflakeConfig
-from itertools import ifilter
 
 import os
 
+DEFAULT_BACKUP_BUCKET = 'similargroup-backup-retention'
+DEFAULT_PREFIX = '/mrp'
 SUCCESS_MARKER = '_SUCCESS'
 DEFAULT_SUFFIX_FORMAT = '''year=%(year)s/month=%(month)s/day=%(day)s'''
 
@@ -16,7 +15,6 @@ DEFAULT_SUFFIX_FORMAT = '''year=%(year)s/month=%(month)s/day=%(day)s'''
 # it might be more efficient, but do we really want hold s3 constructs here? need to decide
 
 logger = logging.getLogger('data_artifact')
-
 
 def human_size(raw_size):
     scale = 1024
@@ -27,11 +25,6 @@ def human_size(raw_size):
         curr_size, curr_idx = curr_size / scale, curr_idx + 1
 
     return curr_size, sizes[curr_idx]
-
-
-def get_s3_buckets(buckets=None):
-    effective_buckets = buckets or SnowflakeConfig().get_service_name(service_name="da-s3-buckets")
-    return json.loads(effective_buckets)
 
 
 class RangedDataArtifact(object):
@@ -62,11 +55,12 @@ class RangedDataArtifact(object):
 
 
 class DataArtifact(object):
-    def __init__(self, path, required_size=0, required_marker=True, buckets=None):
+    def __init__(self, path, required_size=0, required_marker=True, bucket=DEFAULT_BACKUP_BUCKET, pref=DEFAULT_PREFIX):
         self.raw_path = path
         self.min_required_size = required_size
         self.check_marker = required_marker
-        self.buckets = buckets
+        self.bucket = bucket
+        self.prefix = pref
 
     def _hdfs_size(self):
         if dir_exists_hdfs(self.raw_path):
@@ -74,22 +68,11 @@ class DataArtifact(object):
         else:
             return None
 
-    def _s3_path(self, bucket, prefix):
-        return '%s%s%s' % (bucket, prefix or '', self.raw_path)
+    def _s3_path(self, hdfs_path):
+        return '%s%s%s' % (self.bucket, self.prefix or '', hdfs_path)
 
-    def _resolve_s3_size(self):
-        path = self._resolve_s3_path_by_size()
-        return size_on_s3('s3://%s' % path) if path else 0
-
-    def _resolve_s3_path_by_size(self):
-        def paths_by_buckets():
-            bucks = get_s3_buckets(self.buckets)
-            return [self._s3_path(b.get('name'), b.get('prefix')) for b in bucks]
-
-        def filter_by_size(path):
-            return size_on_s3('s3://%s' % path) > 0
-
-        return next(ifilter(filter_by_size, paths_by_buckets()), None)
+    def _s3_size(self):
+        return size_on_s3('s3://%s' % self._s3_path(self.raw_path))
 
     @property
     def actual_size(self):
@@ -97,7 +80,7 @@ class DataArtifact(object):
         if hdfs_size is not None:
             return hdfs_size
         else:
-            return self._resolve_s3_size()
+            return self._s3_size()
 
     def check_size(self):
         return self.actual_size >= self.min_required_size
@@ -111,31 +94,25 @@ class DataArtifact(object):
             if self.check_marker and not exists_hdfs(os.path.join(self.raw_path, SUCCESS_MARKER)):
                 check_marker_ok = False
         else:
-            effective_s3_path = self._resolve_s3_path_by_size()
-            logger.info('Checking that dir %s on s3 is larger than %d...' % (effective_s3_path, self.min_required_size))
-            effective_size = size_on_s3('s3://%s' % effective_s3_path)
-            if self.check_marker and not exists_s3(os.path.join('s3://%s' % effective_s3_path, SUCCESS_MARKER)):
+            logger.info('Checking that dir %s on s3 is larger than %d...' % (self._s3_path(self.raw_path), self.min_required_size))
+            effective_size = self._s3_size()
+            if self.check_marker and not exists_s3(os.path.join('s3://%s' % self._s3_path(self.raw_path), SUCCESS_MARKER)):
                 check_marker_ok = False
 
         for reporter in reporters:
             # TODO decide how to treat the distinction of data found on hdfs/s3
             reporter.report_lineage('input', {self.raw_path: effective_size})
 
-        resolved_path = self.resolved_path
-        assert effective_size >= self.min_required_size, \
-            '%s data is not valid at %s. size is %s, required %s' % (direction, resolved_path, human_size(effective_size), human_size(self.min_required_size))
-        assert check_marker_ok, 'no success marker was found at path %s' % resolved_path
-
+        assert effective_size >= self.min_required_size, '%s data is not valid at %s. size is %s, required %s' % (direction, self.raw_path, human_size(effective_size), human_size(self.min_required_size))
+        assert check_marker_ok, 'no success marker at %s for raw_path %s' % (self.resolved_path, self.raw_path)
         if max_size is not None:
-            assert effective_size >= self.min_required_size, \
-                'requested threshold too small for %s data  at %s. size is %s, required %s' % (direction, resolved_path, human_size(effective_size), human_size(self.min_required_size))
-        logger.info('Data validity checks passed for path %s' % resolved_path)
+            assert effective_size >= self.min_required_size, 'requested threshold too small for %s data  at %s. size is %s, required %s' % (direction, self.resolved_path, human_size(effective_size), human_size(self.min_required_size))
+        logger.info('check passed')
 
     def assert_input_validity(self, *reporters):
         self._assert_data_validity('input', None, *reporters)
 
     STRICT_SIZE_THRESHOLD = 30
-
     def assert_output_validity(self, is_strict=False, *reporters):
         self._assert_data_validity('outupt', self.min_required_size * DataArtifact.STRICT_SIZE_THRESHOLD if is_strict else None, *reporters)
 
@@ -145,5 +122,9 @@ class DataArtifact(object):
         if hdfs_size is not None:
             return self.raw_path
         else:
-            path = self._resolve_s3_path_by_size()
-            return None if not path else "s3a://%s" % path
+            s3_size = self._s3_size()
+            if s3_size > 0:
+            #if s3_size > self.min_required_size and (not self.check_marker or exists_s3(os.path.join('s3://%s' % _s3_path(self.raw_path), SUCCESS_MARKER))):
+                return 's3a://%s' % self._s3_path(self.raw_path)
+            else:
+                return None
