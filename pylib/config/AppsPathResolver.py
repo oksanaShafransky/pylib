08d9e000bfb84ba4fc09595bad1d49c1027e191a
raphael.peretz <raphael.peretz@similarweb.com>
2019-11-13 10:05:54 +0200

    Add aggregations

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -9,26 +9,51 @@ class AppsPathResolver(object):
     apps_paths = {
         'app_country_source_agg': {'main_path': "daily/aggregations/aggKey=AppCountrySourceKey", 'size': 1e9,
                                    'marker': True, 'path_type': "daily"},
+
         'extractor_1001': {'main_path': "daily/extractors/extracted-metric-data/rtype=1001", 'size': 7e10,
                            'marker': True, 'path_type': "daily"},
+
         'extractor_1003': {'main_path': "daily/extractors/extracted-metric-data/rtype=1003", 'size': 2e9,
                            'marker': True, 'path_type': "daily"},
+
         'extractor_1005': {'main_path': "daily/extractors/extracted-metric-data/rtype=1005", 'size': 9e8,
                            'marker': True, 'path_type': "daily"},
+
         'extractor_1009': {'main_path': "daily/extractors/extracted-metric-data/rtype=1009", 'size': 2e9,
                            'marker': True, 'path_type': "daily"},
+
         'extractor_1010': {'main_path': "daily/extractors/extracted-metric-data/rtype=1010", 'size': 9e8,
                            'marker': True, 'path_type': "daily"},
+
         'grouping_1001_report_parquet': {'main_path': "stats-mobile/parquet/rtype=R1001", 'size': 8e10, 'marker': True,
                                          'path_type': "daily"},
+
         'grouping_1003_report_parquet': {'main_path': "stats-mobile/parquet/rtype=R1003", 'size': 2e9, 'marker': True,
                                          'path_type': "daily"},
+
         'grouping_1005_report_parquet': {'main_path': "stats-mobile/parquet/rtype=R1005", 'size': 1e9, 'marker': True,
                                          'path_type': "daily"},
+
         'grouping_1009_report_parquet': {'main_path': "stats-mobile/parquet/rtype=R1009", 'size': 5e9, 'marker': True,
                                          'path_type': "daily"},
 
         'grouping_1010_report_parquet': {'main_path': "stats-mobile/parquet/rtype=R1010", 'size': 2e11, 'marker': True,
+                                         'path_type': "daily"},
+
+        'agg_app_country_source_1009_key': {'main_path': "daily/aggregations/aggKey=AppCountrySource1009Key", 'size': 1e9, 'marker': True,
+                                         'path_type': "daily"},
+
+        'agg_app_country_source_key': {'main_path': "daily/aggregations/aggKey=AppCountrySourceKey", 'size': 1e9, 'marker': True,
+                                         'path_type': "daily"},
+
+        'agg_country_source_1009_key': {'main_path': "daily/aggregations/aggKey=CountrySource1009Key", 'size': 3e5, 'marker': True,
+                                         'path_type': "daily"},
+
+        'agg_country_source_key': {'main_path': "daily/aggregations/aggKey=CountrySourceKey", 'size': 3e5,
+                                        'marker': True,
+                                        'path_type': "daily"},
+
+        'agg_app_country_source_joined_key': {'main_path': "daily/aggregations/aggKey=AppCountrySourceJoinedKey", 'size': 8e9, 'marker': True,
                                          'path_type': "daily"}
     }
 
@@ -104,6 +129,23 @@ class AppsPathResolver(object):
 
     def get_grouping_1010_report_parquet(self, in_or_out):
         return self.__create_app_path_object(self.__get_base_dir(in_or_out), AppsPathResolver.apps_paths['grouping_1010_report_parquet'])
+
+    def get_agg_app_country_source_1009_key(self, in_or_out):
+        return self.__create_app_path_object(self.__get_android_apps_analytics_base(in_or_out), AppsPathResolver.apps_paths['agg_app_country_source_1009_key'])
+
+    def get_agg_app_country_source_key(self, in_or_out):
+        return self.__create_app_path_object(self.__get_android_apps_analytics_base(in_or_out), AppsPathResolver.apps_paths['agg_app_country_source_key'])
+
+    def get_agg_country_source_1009_key(self, in_or_out):
+        return self.__create_app_path_object(self.__get_android_apps_analytics_base(in_or_out), AppsPathResolver.apps_paths['agg_country_source_1009_key'])
+
+    def get_agg_country_source_key(self, in_or_out):
+        return self.__create_app_path_object(self.__get_android_apps_analytics_base(in_or_out), AppsPathResolver.apps_paths['agg_country_source_key'])
+
+    def agg_app_country_source_joined_key(self, in_or_out):
+        return self.__create_app_path_object(self.__get_android_apps_analytics_base(in_or_out), AppsPathResolver.apps_paths['agg_app_country_source_joined_key'])
+
+
 
 
 
