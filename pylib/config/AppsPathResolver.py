2d52a6e62aeeaf5dcada98b1b5ed91f0e8910d1e
raphael.peretz <raphael.peretz@similarweb.com>
2020-03-08 14:34:30 +0200

    Reduce 1009 group file

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -110,7 +110,7 @@ class AppsPathResolver(object):
                                              'marker': True,
                                              'path_type': "daily"},
 
-            'grouping_1009_report_parquet': {'main_path': "stats-mobile/parquet/rtype=R1009", 'size': 4.6 * GB,
+            'grouping_1009_report_parquet': {'main_path': "stats-mobile/parquet/rtype=R1009", 'size': 3.5 * GB,
                                              'marker': True,
                                              'path_type': "daily"},
 
