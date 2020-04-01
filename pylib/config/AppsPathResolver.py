3107f0318427798654111e0cb206c90da7391ac8
chen.rais <chen.rais@similarweb.com>
2020-04-01 16:13:30 +0300

    reduce app_downloads_alpha from 95 to 80

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -200,7 +200,7 @@ class AppsPathResolver(object):
 
             'app_downloads_alph': {
                 'main_path': "daily/estimate/app-downloads-alph/estkey=AppCountryKey",
-                'size': 95 * MB, 'marker': True,
+                'size': 80 * MB, 'marker': True,
                 'path_type': "daily"},
 
             'app_engagement_realnumbers': {
