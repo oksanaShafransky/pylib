4558e51113e979edb927fc580b3505318eddb486
chen.rais <chen.rais@similarweb.com>
2020-04-21 08:48:58 +0300

    add appCountrySourceDaysBack aggregation

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -92,7 +92,7 @@ class AppsPathResolver(object):
             'extractor_1003': {'main_path': "daily/extractors/extracted-metric-data/rtype=R1003", 'size': 500 * MB,
                                'marker': True, 'path_type': "daily"},
 
-            'extractor_1005': {'main_path': "daily/extractors/extracted-metric-data/rtype=R1005", 'size': 400 * MB,
+            'extractor_1005': {'main_path': "daily/extractors/extracted-metric-data/rtype=R1005", 'size': 200 * MB,
                                'marker': True, 'path_type': "daily"},
 
             'extractor_1008': {'main_path': "daily/extractors/extracted-metric-data/rtype=R1008", 'size': 50 * MB,
