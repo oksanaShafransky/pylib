84d720669cd60e5459b1d6d75d7c8f7fecb8edd9
chen.rais <chen.rais@similarweb.com>
2020-04-26 12:03:05 +0300

    reduce output check for 1001 from 30GB to 15GB

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -86,7 +86,7 @@ class AppsPathResolver(object):
             'app_country_source_agg': {'main_path': "daily/aggregations/aggKey=AppCountrySourceKey", 'size': 0.9 * GB,
                                        'marker': True, 'path_type': "daily"},
 
-            'extractor_1001': {'main_path': "daily/extractors/extracted-metric-data/rtype=R1001", 'size': 30 * GB,
+            'extractor_1001': {'main_path': "daily/extractors/extracted-metric-data/rtype=R1001", 'size': 15 * GB,
                                'marker': True, 'path_type': "daily"},
 
             'extractor_1003': {'main_path': "daily/extractors/extracted-metric-data/rtype=R1003", 'size': 500 * MB,
