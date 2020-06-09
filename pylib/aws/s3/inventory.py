c673549fb2b03d0adfc0d7e50229048c9672b9aa
maor shechter <maor.shechter@similarweb.com>
2020-06-09 10:27:54 +0300

    add validate=False param to get_bucket for supporting access to remote s3 where we have read permissions to specific path but no head permission to root

--- pylib/aws/s3/inventory.py
+++ pylib/aws/s3/inventory.py
@@ -5,7 +5,9 @@ from pylib.aws.s3 import parse_s3_url
 def get_size(s3_url):
     s3_conn = boto.connect_s3()
     bucketname, path = parse_s3_url(s3_url)
-    bucket = s3_conn.get_bucket(bucketname)
+    # validate=False because there are some cases when we do not have head permission but read permissions
+    # for specific paths (remote s3).. if bucket doesnt exist the list method will fail
+    bucket = s3_conn.get_bucket(bucketname, validate=False)
 
     return sum([f.size for f in bucket.list(path)])
 
@@ -13,6 +15,6 @@ def get_size(s3_url):
 def does_exist(s3_url):
     s3_conn = boto.connect_s3()
     bucketname, filename = parse_s3_url(s3_url)
-    bucket = s3_conn.get_bucket(bucketname)
+    bucket = s3_conn.get_bucket(bucketname, validate=False)
 
     return bucket.get_key(filename) is not None
