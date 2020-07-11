2d5e9ef7324a189935a105627894450aea4f39ab
raphael.peretz <raphael.peretz@similarweb.com>
2020-07-11 11:32:13 +0300

    Some partitions has no SUCCESS marker

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -146,7 +146,7 @@ class AppsPathResolver(object):
                                              'path_type': "daily"},
 
             'grouping_1005_report_parquet': {'main_path': "stats-mobile/parquet/rtype=R1005", 'size': 400 * MB,
-                                             'marker': True,
+                                             'marker': False, #TODO revert to True.
                                              'path_type': "daily"},
 
             'grouping_1008_report_parquet': {'main_path': "stats-mobile/parquet/rtype=R1008", 'size': 100 * MB,
