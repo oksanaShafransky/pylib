014aa74a98f00d703a6bffb334b724f68aae1d54
Lior Itzhak <lior.itzhak@swnet.corp>
2020-08-13 15:22:37 +0300

    add pre-upgrade code section that should be compatible with both pycoutry versions (1.12 and 18.7.12)

--- pylib/sw_jobs/mw_conf.py
+++ pylib/sw_jobs/mw_conf.py
@@ -2,10 +2,14 @@ __author__ = 'Amit'
 
 from pycountry import countries
 from pylib.sw_config.bigdata_kv import get_kv
+import logging
 
+logger = logging.getLogger("mw_conf")
 
-# default_conf = {'pylib.sw_config.mock.DictProxy': {'services/mobile-web/env/main/countries': '840'}}
-
+# import sys ;logger.setLevel(logging.DEBUG); handler = logging.StreamHandler(sys.stdout) ;
+# handler.setLevel( logging.DEBUG)
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# handler.setFormatter(formatter) ; logger.addHandler(handler)
 
 class MobileWebConfig(object):
 
@@ -17,6 +21,15 @@ class MobileWebConfig(object):
     @property
     def countries(self):
         if not self._countries:
-            self._countries = dict([(country_code, countries.get(numeric='%s' % country_code.zfill(3)).alpha2)
-                                    for country_code in self.conf.get('%s/countries' % self.root).split(',')])
+            # TODO check that all of the envs. updated to pycontry version 18.12.8 and remove this check
+            #  REMOVE THE LOG!
+            if hasattr(countries.get(numeric='840'), 'alpha2'):
+                logger.debug('using pycountry v==1.2')
+                self._countries = dict([(country_code, countries.get(numeric='%s' % country_code.zfill(3)).alpha2)
+                                        for country_code in self.conf.get('%s/countries' % self.root).split(',')])
+            else:
+                logger.debug('using pycountry v==18.12.8')
+                self._countries = dict([(country_code, countries.get(numeric='%s' % country_code.zfill(3)).alpha_2)
+                                        for country_code in self.conf.get('%s/countries' % self.root).split(',')])
+
         return self._countries
