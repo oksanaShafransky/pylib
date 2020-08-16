813a479c201a06bbf8f3ce2a8955fc80db3f3566
Lior Itzhak <lior.itzhak@swnet.corp>
2020-08-16 13:30:46 +0300

    add support for both pycountry versions (1.2 and  18.12.8)

--- pylib/sw_jobs/mw_conf.py
+++ pylib/sw_jobs/mw_conf.py
@@ -1,8 +1,10 @@
 __author__ = 'Amit'
 
-import pycountry
+from pycountry import countries
 from pylib.sw_config.bigdata_kv import get_kv
+import logging
 
+logger = logging.getLogger("mw_conf")
 
 # import sys ;logger.setLevel(logging.DEBUG); handler = logging.StreamHandler(sys.stdout) ;
 # handler.setLevel( logging.DEBUG)
@@ -16,11 +18,21 @@ class MobileWebConfig(object):
         self.conf = kv_provider or get_kv(snowflake_env=snowflake_env)
         self._countries = {}
 
-
     @property
     def countries(self):
         if not self._countries:
-            self._countries = dict([(country_code, pycountry.countries.get(numeric='%s' % country_code.zfill(3)).alpha2)
-                                    for country_code in self.conf.get('%s/countries' % self.root).split(',')])
+            # TODO check that all of the envs. updated to pycontry version 18.12.8 and remove this check
+            #  REMOVE THE LOG!
+            if hasattr(countries.get(numeric='840'), 'alpha2'):
+                logger.debug('using pycountry v==1.2')
+                self._countries = dict([(country_code, countries.get(numeric='%s' % country_code.zfill(3)).alpha2)
+                                        for country_code in self.conf.get('%s/countries' % self.root).split(',')])
+            else:
+                logger.debug('using pycountry v==18.12.8')
+                self._countries = dict([(country_code, countries.get(numeric='%s' % country_code.zfill(3)).alpha_2)
+                                        for country_code in self.conf.get('%s/countries' % self.root).split(',')])
 
         return self._countries
+
+
+
