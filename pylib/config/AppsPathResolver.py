e0a9aa37994a8c217ad3448ecbf852fbfaafd3b4
omer berkman <omer.berkman@similarweb.com>
2021-02-04 12:00:28 +0200

    ww_store_download_panel_country_share_est: reduce output size threshold from 400 to 395

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -192,7 +192,7 @@ class AppsPathResolver(object):
 
             'ww_store_download_panel_country_share_est': {
                 'main_path': "daily/downloads/store_downloads/estimation/est-panel-country-share/estKey=AppCountryKey",
-                'size': 400 * MB,
+                'size': 395 * MB,
                 'marker': True, 'path_type': "daily"},
 
             'ww_store_download_app_delta': {
