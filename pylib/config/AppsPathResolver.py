acf8ed26633e0325385d36012ff9fe0bc913c93a
omer berkman <omer.berkman@similarweb.com>
2021-02-12 10:01:53 +0200

    dau_prior reduced from 500 to 490MB, downloads_app_country_country_source_agg from 6 to 5.8GB

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -101,7 +101,7 @@ class AppsPathResolver(object):
                              'marker': True, 'path_type': "daily"},#We Can't track size here in a good way.
 
             'downloads_app_country_country_source_agg': {'main_path': "daily/downloads/aggregations/aggKey=AppCountryCountrySourceKey",
-                                                         'size': 5.75 * GB,
+                                                         'size': 5.8 * GB,
                                                          'marker': True, 'path_type': "daily"},
 
             'downloads_app_country_delta_key_agg': {'main_path': "daily/downloads/aggregations/aggKey=AppCountryDeltaKey",
