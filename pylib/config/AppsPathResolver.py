8b52051b85e116f5486cce7891493cebcf6ee9b0
omer berkman <omer.berkman@similarweb.com>
2021-02-16 09:20:50 +0200

    ls extractors

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -278,10 +278,16 @@ class AppsPathResolver(object):
                                      'marker': False, 'path_type': "daily"},
 
             'extractor_ga_daily': {'main_path': 'raw-s2s/extractor_ga_daily', 'size': 10 * MB,
-                                   'marker': False, 'path_type': "daily"},
+                                   'marker': False, 'path_type': "monthly"},
+
+            'extractor_ga_monthly': {'main_path': 'raw-s2s/extractor_ga_monthly', 'size': 10 * MB,
+                                     'marker': False, 'path_type': "monthly"},
 
             'extractor_kwh_daily': {'main_path': 'raw-s2s/extractor_kwh_daily', 'size': 10 * MB,
-                                    'marker': False, 'path_type': "daily"},
+                                    'marker': False, 'path_type': "monthly"},
+
+            'extractor_kwh_monthly': {'main_path': 'raw-s2s/extractor_kwh_monthly', 'size': 10 * MB,
+                                      'marker': False, 'path_type': "monthly"},
 
             'mfour_apps_sessions': {'main_path': 'raw-s2s/mfour-apps-sessions', 'size': 80 * MB,
                                     'marker': False, 'path_type': "daily"},
@@ -596,9 +602,17 @@ class AppsPathResolver(object):
             return self.__create_app_path_object(self.__get_android_apps_analytics_base(in_or_out, path_prefix),
                                                  self.apps_paths['extractor_ga_daily'], path_suffix, in_or_out)
 
+    def get_extractor_ga_monthly(self, in_or_out, path_prefix=None, path_suffix=None):
+        return self.__create_app_path_object(self.__get_android_apps_analytics_base(in_or_out, path_prefix),
+                                             self.apps_paths['extractor_ga_monthly'], path_suffix, in_or_out)
+
     def get_extractor_kwh_daily(self, in_or_out, path_prefix=None, path_suffix=None):
         return self.__create_app_path_object(self.__get_android_apps_analytics_base(in_or_out, path_prefix),
                                              self.apps_paths['extractor_kwh_daily'], path_suffix, in_or_out)
+
+    def get_extractor_kwh_monthly(self, in_or_out, path_prefix=None, path_suffix=None):
+        return self.__create_app_path_object(self.__get_android_apps_analytics_base(in_or_out, path_prefix),
+                                             self.apps_paths['extractor_kwh_monthly'], path_suffix, in_or_out)
 
     def get_mfour_apps_sessions(self, in_or_out, path_prefix=None, path_suffix=None):
         return self.__create_app_path_object(self.__get_base_dir(in_or_out, path_prefix),
