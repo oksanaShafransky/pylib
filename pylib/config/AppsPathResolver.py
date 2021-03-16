c896d6c2adec7523cb2b5872cb13a229749fdc0b
maor shechter <maor.shechter@similarweb.com>
2021-03-16 16:08:48 +0200

    add 1003 upsolver path

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -340,6 +340,10 @@ class AppsPathResolver(object):
                                              'marker': False,
                                              'path_type': "daily"},
 
+            'grouping_1003_report_parquet_upsolver': {'main_path': "stats-mobile/parquet_adjusted/rtype=R1003", 'size': 1 * MB,
+                                                      'marker': False,
+                                                      'path_type': "daily"},
+
             'grouping_1005_report_parquet': {'main_path': "stats-mobile/parquet/rtype=R1005", 'size': 400 * MB,
                                              'marker': False, #TODO revert to True.
                                              'path_type': "daily"},
@@ -700,6 +704,10 @@ class AppsPathResolver(object):
     def get_grouping_1003_report_parquet(self, in_or_out, path_prefix=None, path_suffix=None):
         return self.__create_app_path_object(self.__get_base_dir(in_or_out, path_prefix),
                                              self.apps_paths['grouping_1003_report_parquet'], path_suffix, in_or_out)
+
+    def get_grouping_1003_report_parquet_upsolver(self, in_or_out, path_prefix=None, path_suffix="is_valid=true"):
+        return self.__create_app_path_object(self.__get_base_dir(in_or_out, path_prefix),
+                                             self.apps_paths['grouping_1003_report_parquet_upsolver'], path_suffix, in_or_out)
 
     def get_grouping_1005_report_parquet(self, in_or_out, path_prefix=None, path_suffix=None):
         return self.__create_app_path_object(self.__get_base_dir(in_or_out, path_prefix),
