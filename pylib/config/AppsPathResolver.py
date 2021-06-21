c3774c17bf1545ae6de53f4da0695f071e1ffb95
yaaqov.brauner <yaaqov.brauner@similarweb.com>
2021-06-21 22:04:09 +0300

    add new user features path

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -313,6 +313,11 @@ class AppsPathResolver(object):
             'new_user_alpha_estimation': {'main_path': "daily/downloads/new_users/estimation/app-downloads-alph/estkey=AppCountryKey",
                                           'size': 150 * MB,
                                           'marker': True, 'path_type': "daily"},
+
+            'new_user_features': {'main_path': "daily/downloads/new_users/features",
+                                          'size': 150 * MB,
+                                          'marker': True, 'path_type': "daily"},
+
             'installs_alpha_estimation': {
                 'main_path': "daily/downloads/installs/estimation/app-downloads-alph/estkey=AppCountryKey",
                 'size': 150 * MB,
@@ -1025,6 +1030,10 @@ class AppsPathResolver(object):
     def get_new_user_alpha_estimation(self, in_or_out, path_prefix=None, path_suffix=None):
         return self.__create_app_path_object(self.__get_android_apps_analytics_base(in_or_out, path_prefix),
                                              self.apps_paths['new_user_alpha_estimation'], path_suffix, in_or_out)
+
+    def get_new_users_features(self, in_or_out, path_prefix=None, path_suffix=None):
+        return self.__create_app_path_object(self.__get_android_apps_analytics_base(in_or_out, path_prefix),
+                                             self.apps_paths['new_user_features'], path_suffix, in_or_out)
 
     def get_installs_alpha_estimation(self, in_or_out, path_prefix=None, path_suffix=None):
         return self.__create_app_path_object(self.__get_android_apps_analytics_base(in_or_out, path_prefix),
