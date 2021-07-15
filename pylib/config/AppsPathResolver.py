ae2d6a0f1548c01d75d17ea55b32ec2d69307923
omer berkman <omer.berkman@similarweb.com>
2021-07-15 17:49:38 +0300

    matching-model

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -113,9 +113,12 @@ class AppsPathResolver(object):
             'matching-image-features-android': {'main_path': "apps-matching/image-features-android",
                                             'size': 20 * MB,
                                             'marker': True, 'path_type': "daily"},
+            'matching-model': {'main_path': "apps-matching/model",
+                               'size': 1 * MB,
+                               'marker': True, 'path_type': "daily"},
             'matching-predict': {'main_path': "apps-matching/predict",
-                                     'size': 5 * MB,
-                                     'marker': True, 'path_type': "daily"},
+                                 'size': 5 * MB,
+                                 'marker': True, 'path_type': "daily"},
             'matching-tests': {'main_path': "apps-matching/tests",
                                      'size': 5 * MB,
                                      'marker': True, 'path_type': "daily"},
@@ -1222,15 +1225,18 @@ class AppsPathResolver(object):
     def get_matching_learning_set(self, in_or_out, path_prefix=None, path_suffix=None):
         return self.__create_app_path_object(self.__get_android_apps_analytics_base(in_or_out, path_prefix),
                                              self.apps_paths['matching-learning-set'], path_suffix, in_or_out)
-    def get_matching_training_data(self, in_or_out, path_prefix=None, path_suffix=None):
-        return self.__create_app_path_object(self.__get_android_apps_analytics_base(in_or_out, path_prefix),
-                                             self.apps_paths['matching-training-data'], path_suffix, in_or_out)
     def get_matching_image_features_ios(self, in_or_out, path_prefix=None, path_suffix=None):
         return self.__create_app_path_object(self.__get_android_apps_analytics_base(in_or_out, path_prefix),
                                              self.apps_paths['matching-image-features-ios'], path_suffix, in_or_out)
     def get_matching_image_features_android(self, in_or_out, path_prefix=None, path_suffix=None):
         return self.__create_app_path_object(self.__get_android_apps_analytics_base(in_or_out, path_prefix),
                                              self.apps_paths['matching-image-features-android'], path_suffix, in_or_out)
+    def get_matching_training_data(self, in_or_out, path_prefix=None, path_suffix=None):
+        return self.__create_app_path_object(self.__get_android_apps_analytics_base(in_or_out, path_prefix),
+                                             self.apps_paths['matching-training-data'], path_suffix, in_or_out)
+    def get_matching_model(self, in_or_out, path_prefix=None, path_suffix=None):
+        return self.__create_app_path_object(self.__get_android_apps_analytics_base(in_or_out, path_prefix),
+                                             self.apps_paths['matching-model'], path_suffix, in_or_out)
     def get_matching_predict(self, in_or_out, path_prefix=None, path_suffix=None):
         return self.__create_app_path_object(self.__get_android_apps_analytics_base(in_or_out, path_prefix),
                                              self.apps_paths['matching-predict'], path_suffix, in_or_out)
