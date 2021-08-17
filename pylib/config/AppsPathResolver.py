6ad89f37e5725d4dc120706da06b6636caa42648
omer berkman <omer.berkman@similarweb.com>
2021-08-17 10:03:47 +0300

    reduced ptft size validations

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -507,7 +507,7 @@ class AppsPathResolver(object):
                                              'marker': False, #TODO revert to True.
                                              'path_type': "daily"},
 
-            'grouping_1005_report_parquet_upsolver': {'main_path': "stats-mobile/parquet_adjusted/rtype=R1005", 'size': 330 * MB,
+            'grouping_1005_report_parquet_upsolver': {'main_path': "stats-mobile/parquet_adjusted/rtype=R1005", 'size': 320 * MB,
                                              'marker': False,
                                              'path_type': "daily"},
 
