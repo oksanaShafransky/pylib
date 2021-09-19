22ec371e211caabd620d563d01fe844d98f4cb44
omer berkman <omer.berkman@similarweb.com>
2021-09-19 08:32:26 +0300

    daily/extractors/extracted-metric-data/rtype=R1001

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -448,7 +448,7 @@ class AppsPathResolver(object):
             'app_country_source_agg': {'main_path': "daily/aggregations/aggKey=AppCountrySourceKey", 'size': 0.9 * GiB,
                                        'marker': True, 'path_type': "daily"},
 
-            'extractor_1001': {'main_path': "daily/extractors/extracted-metric-data/rtype=R1001", 'size': 7 * GiB,
+            'extractor_1001': {'main_path': "daily/extractors/extracted-metric-data/rtype=R1001", 'size': 7 * GB,
                                'marker': True, 'path_type': "daily"},
 
             'extractor_1003': {'main_path': "daily/extractors/extracted-metric-data/rtype=R1003", 'size': 200 * MiB,
