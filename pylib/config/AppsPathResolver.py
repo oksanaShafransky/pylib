70ca1a939ebe9b34549176909eaa9888a1f9d2bf
Oksana Shafransky <oksana.shafransky@similarweb.com>
2021-10-24 09:43:06 +0300

    Fix 1001-extractor size

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -471,7 +471,7 @@ class AppsPathResolver(object):
             'app_country_source_agg': {'main_path': "daily/aggregations/aggKey=AppCountrySourceKey", 'size': 0.9 * GiB,
                                        'marker': True, 'path_type': "daily"},
 
-            'extractor_1001': {'main_path': "daily/extractors/extracted-metric-data/rtype=R1001", 'size': 6.7 * GB,
+            'extractor_1001': {'main_path': "daily/extractors/extracted-metric-data/rtype=R1001", 'size': 6.5 * GB,
                                'marker': True, 'path_type': "daily"},
 
             'extractor_1003': {'main_path': "daily/extractors/extracted-metric-data/rtype=R1003", 'size': 200 * MiB,
