a9165dd28ac79a7470000cc67abb851e987aacbb
omer berkman <omer.berkman@similarweb.com>
2021-10-26 08:18:28 +0300

    apps usage

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -351,7 +351,7 @@ class AppsPathResolver(object):
 
             # Usage
             'usage_agg_app_country': {'main_path': "daily/usage/agg",
-                             'size': 13.5 * MB,
+                             'size': 13.2 * MB,
                              'marker': True, 'path_type': "daily"},
             'usage_prior': {'main_path': "daily/usage/prior",
                             'size': 25 * MB,
