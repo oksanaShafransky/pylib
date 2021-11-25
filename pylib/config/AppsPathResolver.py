19c9586e099c213282d3fb7537cd37d4a260762e
omer berkman <omer.berkman@similarweb.com>
2021-11-25 13:04:14 +0200

    usage/prior

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -389,7 +389,7 @@ class AppsPathResolver(object):
                              'size': 5.5 * MB,
                              'marker': True, 'path_type': "daily"},
             'usage_prior': {'main_path': "daily/usage/prior",
-                            'size': 15 * MB,
+                            'size': 14 * MB,
                             'marker': True, 'path_type': "daily"},
             'usage_estimation': {'main_path': "daily/usage/estimation",
                                  'size': 13 * MB,
