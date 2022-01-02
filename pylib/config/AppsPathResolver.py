d034123e7accb8dc8f97b82db16701e86e77f53c
omer berkman <omer.berkman@similarweb.com>
2022-01-02 11:08:19 +0200

    grouping_1005_report_parquet_upsolver

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -644,7 +644,7 @@ class AppsPathResolver(object):
                                              'marker': False, #TODO revert to True.
                                              'path_type': "daily"},
 
-            'grouping_1005_report_parquet_upsolver': {'main_path': "stats-mobile/parquet_adjusted/rtype=R1005", 'size': 170 * MB,
+            'grouping_1005_report_parquet_upsolver': {'main_path': "stats-mobile/parquet_adjusted/rtype=R1005", 'size': 165 * MB,
                                              'marker': False,
                                              'path_type': "daily"},
 
