82d23656b0cfc5f4867b7963abe8e04d1951c210
Carl Veksler <carl.veksler@similarweb.com>
2022-02-10 19:09:07 +0200

    changed requred size for 'app-scores'

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -855,7 +855,7 @@ class AppsPathResolver(object):
             # Snapshot/WindowF
             'app_scores': {
                 'main_path': "%(mode)s/app-scores/type=%(mode_type)s" % {'mode': self.ti.mode, 'mode_type': self.ti.mode_type},
-                'size': 100 * MiB, 'marker': False,  # Size close for both window ,and snapshot
+                'size': 97 * MiB, 'marker': False,  # Size close for both window ,and snapshot
                 'path_type': self.ti.mode_type},
 
             'app_scores_with_info': {
