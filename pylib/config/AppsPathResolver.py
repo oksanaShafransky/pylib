e17d18786c6189339cd0621de77732c6b59eddb5
Carl Veksler <carl.veksler@similarweb.com>
2022-04-02 13:04:02 +0300

    reduced dau/aggregations/new_calc/aggKey=AppCountrySourceJoinedKey required size 1GiB -> 1.05GB

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -398,11 +398,11 @@ class AppsPathResolver(object):
                                             'marker': True, 'path_type': "daily"},
 
             'dau_join_agg': {'main_path': "daily/dau/aggregations/aggKey=AppCountrySourceJoinedKey",
-                             'size': 1.05 * GB,
+                             'size': 1 * GiB,
                              'marker': True, 'path_type': "daily"},
 
             'dau_calc_join_agg': {'main_path': "daily/dau/aggregations/new_calc/aggKey=AppCountrySourceJoinedKey",
-                             'size': 1 * GiB,
+                             'size': 1.05 * GB,
                              'marker': True, 'path_type': "daily"},
 
             'dau_for_ptft': {'main_path': "daily/dau/pre-estimate/dau-for-ptft",
