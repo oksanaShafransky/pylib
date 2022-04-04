56c843fd7514b944b523831e9b2fd3c96da7995d
Oksana Shafransky <oksana.shafransky@similarweb.com>
2022-04-04 15:26:19 +0300

    Fix size for get_dau_calc_app_country_source_join_agg

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -411,7 +411,7 @@ class AppsPathResolver(object):
                              'marker': True, 'path_type': "daily"},
 
             'dau_calc_join_agg': {'main_path': "daily/dau/aggregations/new_calc/aggKey=AppCountrySourceJoinedKey",
-                             'size': 1.05 * GB,
+                             'size': 1 * GB,
                              'marker': True, 'path_type': "daily"},
 
             'dau_for_ptft': {'main_path': "daily/dau/pre-estimate/dau-for-ptft",
