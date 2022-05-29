572bd6c776274ce578e43fdcaaffc0471f2a17e6
Oksana Shafransky <oksana.shafransky@similarweb.com>
2022-05-29 16:35:49 +0300

    daily/downloads/aggregations/new_calc/aggKey=AppCountryCountrySourceKey size changed to 4GB

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -360,7 +360,7 @@ class AppsPathResolver(object):
                                                               'marker': True, 'path_type': "daily"},
 
             'calc_downloads_app_country_country_source_agg': {'main_path': "daily/downloads/aggregations/new_calc/aggKey=AppCountryCountrySourceKey",
-                                                              'size': 4.2 * GB,
+                                                              'size': 4 * GB,
                                                               'marker': True, 'path_type': "daily"},
 
 
