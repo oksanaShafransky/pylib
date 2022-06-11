6dd332050100b5536274f59d174e0af77949c825
Tal Blau <tal.blau@similarweb.com>
2022-06-11 11:53:13 +0300

    update ww_downloads_to_scrape size check from 5.5 to 5.4

--- pylib/config/AppsPathResolver.py
+++ pylib/config/AppsPathResolver.py
@@ -1103,7 +1103,7 @@ class AppsPathResolver(object):
                                    'path_type': "daily"},
 
             'ww_downloads_to_scrape': {'main_path': "scraping/ww-downloads-to-scrape",
-                         'size': 5.5 * MB, 'marker': True,
+                         'size': 5.4 * MB, 'marker': True,
                          'path_type': "daily"},
 
             # store-analysis
